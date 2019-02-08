5f36f86458aecf246ed1af3e032cf2185a0128be
Paul Romano <paul.k.romano@gmail.com>
2019-02-07 21:51:04 -0600

    Update conversion script for ENDF/B-VII.0 data from MCNP
    Use pathlib, f-strings, add photon data

--- convert_mcnp70.py
+++ convert_mcnp70.py
@@ -2,48 +2,53 @@
 
 import argparse
 from collections import defaultdict
-import glob
-import os
+from pathlib import Path
+import sys
 
 import openmc.data
 
 
+# Make sure Python version is sufficient
+assert sys.version_info >= (3, 6), "Python 3.6+ is required"
+
 description = """
 Convert ENDF/B-VII.0 ACE data from the MCNP5/6 distribution into an HDF5 library
 that can be used by OpenMC. This assumes that you have a directory containing
-files named endf70a, endf70b, ..., endf70k, and endf70sab.
+files named endf70[a-k], endf70sab, and mcplib84.
 
 """
+
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
                       argparse.RawDescriptionHelpFormatter):
     pass
 
+
 parser = argparse.ArgumentParser(
     description=description,
     formatter_class=CustomFormatter
 )
-parser.add_argument('-d', '--destination', default='mcnp_endfb70',
+parser.add_argument('-d', '--destination', type=Path, default=Path('mcnp_endfb70'),
                     help='Directory to create new library in')
 parser.add_argument('--libver', choices=['earliest', 'latest'],
                     default='earliest', help="Output HDF5 versioning. Use "
                     "'earliest' for backwards compatibility or 'latest' for "
                     "performance")
-parser.add_argument('mcnpdata', help='Directory containing endf70[a-k] and endf70sab')
+parser.add_argument('mcnpdata', type=Path,
+                    help="Directory containing endf70[a-k], endf70sab, and mcplib")
 args = parser.parse_args()
-assert os.path.isdir(args.mcnpdata)
+assert args.mcnpdata.is_dir()
 
 # Get a list of all neutron ACE files
-endf70 = glob.glob(os.path.join(args.mcnpdata, 'endf70[a-k]'))
+endf70 = args.mcnpdata.glob('endf70[a-k]')
 
 # Create output directory if it doesn't exist
-if not os.path.isdir(args.destination):
-    os.mkdir(args.destination)
+(args.destination / 'photon').mkdir(parents=True, exist_ok=True)
 
 library = openmc.data.DataLibrary()
 
 for path in sorted(endf70):
-    print('Loading data from {}...'.format(path))
+    print(f'Loading data from {path}...')
     lib = openmc.data.ace.Library(path)
 
     # Group together tables for the same nuclide
@@ -54,25 +59,25 @@ for path in sorted(endf70):
 
     for zaid, tables in sorted(tables.items()):
         # Convert first temperature for the table
-        print('Converting: ' + tables[0].name)
+        print(f'Converting: {tables[0].name}')
         data = openmc.data.IncidentNeutron.from_ace(tables[0], 'mcnp')
 
         # For each higher temperature, add cross sections to the existing table
         for table in tables[1:]:
-            print('Adding: ' + table.name)
+            print(f'Adding: {table.name}')
             data.add_temperature_from_ace(table, 'mcnp')
 
         # Export HDF5 file
-        h5_file = os.path.join(args.destination, data.name + '.h5')
-        print('Writing {}...'.format(h5_file))
+        h5_file = args.destination / f'{data.name}.h5'
+        print(f'Writing {h5_file}...')
         data.export_to_hdf5(h5_file, 'w', libver=args.libver)
 
         # Register with library
         library.register_file(h5_file)
 
 # Handle S(a,b) tables
-endf70sab = os.path.join(args.mcnpdata, 'endf70sab')
-if os.path.exists(endf70sab):
+endf70sab = args.mcnpdata / 'endf70sab'
+if endf70sab.exists():
     lib = openmc.data.ace.Library(endf70sab)
 
     # Group together tables for the same nuclide
@@ -83,22 +88,39 @@ if os.path.exists(endf70sab):
 
     for zaid, tables in sorted(tables.items()):
         # Convert first temperature for the table
-        print('Converting: ' + tables[0].name)
+        print(f'Converting: {tables[0].name}')
         data = openmc.data.ThermalScattering.from_ace(tables[0])
 
         # For each higher temperature, add cross sections to the existing table
         for table in tables[1:]:
-            print('Adding: ' + table.name)
+            print(f'Adding: {table.name}')
             data.add_temperature_from_ace(table)
 
         # Export HDF5 file
-        h5_file = os.path.join(args.destination, data.name + '.h5')
-        print('Writing {}...'.format(h5_file))
+        h5_file = args.destination / f'{data.name}.h5'
+        print(f'Writing {h5_file}...')
+        data.export_to_hdf5(h5_file, 'w', libver=args.libver)
+
+        # Register with library
+        library.register_file(h5_file)
+
+# Handle photoatomic data
+mcplib = args.mcnpdata / 'mcplib84'
+if mcplib.exists():
+    lib = openmc.data.ace.Library(mcplib)
+
+    for table in lib.tables:
+        # Convert first temperature for the table
+        print(f'Converting: {table.name}')
+        data = openmc.data.IncidentPhoton.from_ace(table)
+
+        # Export HDF5 file
+        h5_file = args.destination / 'photon' / f'{data.name}.h5'
+        print(f'Writing {h5_file}...')
         data.export_to_hdf5(h5_file, 'w', libver=args.libver)
 
         # Register with library
         library.register_file(h5_file)
 
 # Write cross_sections.xml
-libpath = os.path.join(args.destination, 'cross_sections.xml')
-library.export_to_xml(libpath)
+library.export_to_xml(args.destination / 'cross_sections.xml')
