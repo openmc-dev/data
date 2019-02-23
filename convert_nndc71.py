d73a1ffcd427180e3c4be7f5e06597fe76aaf17a
shimwell <mail@jshimwell.com>
2019-02-23 09:34:36 +0000

    made use of download function in utils to replace code block

--- convert_nndc71.py
+++ convert_nndc71.py
@@ -6,17 +6,17 @@ from NNDC and convert it to an HDF5 library for use with OpenMC. This data is
 used for OpenMC's regression test suite.
 """
 
+import argparse
+import glob
+import hashlib
 import os
 import shutil
 import subprocess
 import sys
 import tarfile
-import glob
-import hashlib
-import argparse
-from urllib.request import urlopen
 
 import openmc.data
+from openmc._utils import download
 
 
 class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
@@ -51,36 +51,8 @@ files_complete = []
 for f in files:
     # Establish connection to URL
     url = base_url + f
-    req = urlopen(url)
-
-    # Get file size from header
-    file_size = req.length
-    downloaded = 0
-
-    # Check if file already downloaded
-    if os.path.exists(f):
-        if os.path.getsize(f) == file_size:
-            print('Skipping ' + f)
-            files_complete.append(f)
-            continue
-        else:
-            overwrite = input('Overwrite {}? ([y]/n) '.format(f))
-            if overwrite.lower().startswith('n'):
-                continue
-
-    # Copy file to disk
-    print('Downloading {}... '.format(f), end='')
-    with open(f, 'wb') as fh:
-        while True:
-            chunk = req.read(block_size)
-            if not chunk: break
-            fh.write(chunk)
-            downloaded += len(chunk)
-            status = '{0:10}  [{1:3.2f}%]'.format(
-                downloaded, downloaded * 100. / file_size)
-            print(status + chr(8)*len(status), end='')
-        print('')
-        files_complete.append(f)
+    downloaded_file = download(url)
+    files_complete.append(f)
 
 # ==============================================================================
 # VERIFY MD5 CHECKSUMS
