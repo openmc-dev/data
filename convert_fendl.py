6d712ca1b9d2554c38c6338617e6463d0a8ecc84
Paul Romano <paul.k.romano@gmail.com>
2019-07-01 07:33:18 -0500

    Use as_browser=True in convert_fendl.py

--- convert_fendl.py
+++ convert_fendl.py
@@ -84,7 +84,8 @@ files_complete = []
 for f in release_details[args.release]['files']:
     # Establish connection to URL
     url = release_details[args.release]['base_url'] + f
-    downloaded_file = download(url, context=ssl._create_unverified_context())
+    downloaded_file = download(url, as_browser=True,
+                               context=ssl._create_unverified_context())
     files_complete.append(downloaded_file)
 
 # ==============================================================================
