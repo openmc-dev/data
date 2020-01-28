05eaba84b3072057ad442b99fc1aeea9a11a4e17
Tim Fry <tim@frys.me.uk>
2020-01-28 14:28:34 +0000

    Replace os.path with Path from pathlib
    Replace os.path with Path from pathlib

--- convert_tendl.py
+++ convert_tendl.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-import glob
-import os
+from pathlib import Path
 import sys
 import tarfile
 
@@ -27,7 +26,7 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('-b', '--batch', action='store_true',
                     help='supresses standard in')
-parser.add_argument('-d', '--destination', default=None,
+parser.add_argument('-d', '--destination', type=Path, default=None,
                     help='Directory to create new library in')
 parser.add_argument('--libver', choices=['earliest', 'latest'],
                     default='latest', help="Output HDF5 versioning. Use "
@@ -41,26 +40,26 @@ args = parser.parse_args()
 
 
 library_name = 'tendl' #this could be added as an argument to allow different libraries to be downloaded
-ace_files_dir = '-'.join([library_name, args.release, 'ace'])
+ace_files_dir = Path('-'.join([library_name, args.release, 'ace']))
 # the destination is decided after the release is know to avoid putting the release in a folder with a misleading name
 if args.destination is None:
-    args.destination = '-'.join([library_name, args.release, 'hdf5'])
+    args.destination = Path('-'.join([library_name, args.release, 'hdf5']))
 
 # This dictionary contains all the unique information about each release. This can be exstened to accommodated new releases
 release_details = {
     '2015': {
         'base_url': 'https://tendl.web.psi.ch/tendl_2015/tar_files/',
         'files': ['ACE-n.tgz'],
-        'neutron_files': os.path.join(ace_files_dir, 'neutron_file', '*', '*', 'lib', 'endf', '*-n.ace'),
-        'metastables': os.path.join(ace_files_dir, 'neutron_file', '*', '*', 'lib', 'endf', '*m-n.ace'),
+        'neutron_files': ace_files_dir.glob('neutron_file/*/*/lib/endf/*-n.ace'),
+        'metastables': ace_files_dir.glob('neutron_file/*/*/lib/endf/*m-n.ace'),
         'compressed_file_size': '5.1 GB',
         'uncompressed_file_size': '40 GB'
     },
     '2017': {
         'base_url': 'https://tendl.web.psi.ch/tendl_2017/tar_files/',
         'files': ['tendl17c.tar.bz2'],
-        'neutron_files': os.path.join(ace_files_dir, 'ace-17', '*'),
-        'metastables': os.path.join(ace_files_dir, 'ace-17', '*m'),
+        'neutron_files': ace_files_dir.glob('ace-17/*'),
+        'metastables': ace_files_dir.glob('ace-17/*m'),
         'compressed_file_size': '2.1 GB',
         'uncompressed_file_size': '14 GB'
     }
@@ -96,16 +95,14 @@ for f in release_details[args.release]['files']:
         continue
 
     # Extract files
-
-    suffix = ''
     with tarfile.open(f, 'r') as tgz:
         print('Extracting {0}...'.format(f))
-        tgz.extractall(path=os.path.join(ace_files_dir, suffix))
+        tgz.extractall(path=ace_files_dir)
 
 # ==============================================================================
 # CHANGE ZAID FOR METASTABLES
 
-metastables = glob.glob(release_details[args.release]['metastables'])
+metastables = release_details[args.release]['metastables']
 for path in metastables:
     print('    Fixing {} (ensure metastable)...'.format(path))
     text = open(path, 'r').read()
@@ -118,29 +115,28 @@ for path in metastables:
 # GENERATE HDF5 LIBRARY -- NEUTRON FILES
 
 # Get a list of all ACE files
-neutron_files = glob.glob(release_details[args.release]['neutron_files'])
+neutron_files = release_details[args.release]['neutron_files']
 
 # Create output directory if it doesn't exist
-if not os.path.isdir(args.destination):
-    os.mkdir(args.destination)
+args.destination.mkdir(parents=True, exist_ok=True)
 
 library = openmc.data.DataLibrary()
 
 for filename in sorted(neutron_files):
 
     # this is a fix for the TENDL-2017 release where the B10 ACE file which has an error on one of the values
-    if library_name == 'tendl' and args.release == '2017' and os.path.basename(filename) == 'B010':
+    if library_name == 'tendl' and args.release == '2017' and filename.name == 'B010':
         text = open(filename, 'r').read()
         if text[423:428] == '86843':
             print('Manual fix for incorrect value in ACE file') # see OpenMC user group issue for more details
             text = ''.join(text[:423])+'86896'+''.join(text[428:])
             open(filename, 'w').write(text)
 
-    print('Converting: ' + filename)
+    print('Converting: ' + str(filename))
     data = openmc.data.IncidentNeutron.from_ace(filename)
 
     # Export HDF5 file
-    h5_file = os.path.join(args.destination, data.name + '.h5')
+    h5_file = args.destination / f'{data.name}.h5'
     print('Writing {}...'.format(h5_file))
     data.export_to_hdf5(h5_file, 'w', libver=args.libver)
 
@@ -148,5 +144,4 @@ for filename in sorted(neutron_files):
     library.register_file(h5_file)
 
 # Write cross_sections.xml
-libpath = os.path.join(args.destination, 'cross_sections.xml')
-library.export_to_xml(libpath)
+library.export_to_xml(args.destination / 'cross_sections.xml')
