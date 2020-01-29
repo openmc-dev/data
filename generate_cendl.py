5a17f84053f4d5385f7827226055d7c3a791dd38
Tim Fry <tim@frys.me.uk>
2020-01-29 12:24:52 +0000

    Replace os.path with Path from pathlib
    Replace os.path with Path from pathlib

--- generate_cendl.py
+++ generate_cendl.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
-import glob
-import os
+from pathlib import Path
 import sys
 import zipfile
 
@@ -27,7 +26,7 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('-b', '--batch', action='store_true',
                     help='supresses standard in')
-parser.add_argument('-d', '--destination', default=None,
+parser.add_argument('-d', '--destination', type=Path, default=None,
                     help='Directory to create new library in')
 parser.add_argument('--libver', choices=['earliest', 'latest'],
                     default='latest', help="Output HDF5 versioning. Use "
@@ -41,18 +40,18 @@ args = parser.parse_args()
 
 
 library_name = 'cendl' #this could be added as an argument to allow different libraries to be downloaded
-endf_files_dir = '-'.join([library_name, args.release, 'endf'])
+endf_files_dir = Path('-'.join([library_name, args.release, 'endf']))
 # the destination is decided after the release is known to avoid putting the release in a folder with a misleading name
 if args.destination is None:
-    args.destination = '-'.join([library_name, args.release, 'hdf5'])
+    args.destination = Path('-'.join([library_name, args.release, 'hdf5']))
 
 # This dictionary contains all the unique information about each release. This can be exstened to accommodated new releases
 release_details = {
     '3.1': {
         'base_url': 'https://www.oecd-nea.org/dbforms/data/eva/evatapes/cendl_31/',
         'files': ['CENDL-31.zip'],
-        'neutron_files': os.path.join(endf_files_dir, '*.C31'),
-        'metastables': os.path.join(endf_files_dir, '*m.C31'),
+        'neutron_files': endf_files_dir.glob('*.C31'),
+        'metastables': endf_files_dir.glob('*m.C31'),
         'compressed_file_size': '0.03 GB',
         'uncompressed_file_size': '0.4 GB'
     }
@@ -89,41 +88,39 @@ for f in release_details[args.release]['files']:
 
     # Extract files
 
-    suffix = ''
     with zipfile.ZipFile(f) as zf:
         print('Extracting {0}...'.format(f))
-        zf.extractall(path=os.path.join(endf_files_dir, suffix))
+        zf.extractall(path=endf_files_dir)
 
 
 # ==============================================================================
 # GENERATE HDF5 LIBRARY -- NEUTRON FILES
 
 # Get a list of all ENDF files
-neutron_files = glob.glob(release_details[args.release]['neutron_files'])
+neutron_files = release_details[args.release]['neutron_files']
 
 # Create output directory if it doesn't exist
-if not os.path.isdir(args.destination):
-    os.mkdir(args.destination)
+args.destination.mkdir(parents=True, exist_ok=True)
 
 library = openmc.data.DataLibrary()
 
 for filename in sorted(neutron_files):
 
     # this is a fix for the CENDL 3.1 release where the 22-Ti-047.C31 and 5-B-010.C31 files contain non-ASCII characters
-    if library_name == 'cendl' and args.release == '3.1' and os.path.basename(filename) in  ['22-Ti-047.C31','5-B-010.C31']:
+    if library_name == 'cendl' and args.release == '3.1' and filename.name in  ['22-Ti-047.C31','5-B-010.C31']:
         print('Manual fix for incorrect value in ENDF file')
         text = open(filename, 'rb').read().decode('utf-8','ignore').split('\r\n')
-        if os.path.basename(filename) == '22-Ti-047.C31':
+        if filename.name == '22-Ti-047.C31':
             text[205] = ' 8) YUAN Junqian,WANG Yongchang,etc.               ,16,(1),57,92012228 1451  205'
-        if os.path.basename(filename) == '5-B-010.C31':
+        if filename.name == '5-B-010.C31':
             text[203] = '21)   Day R.B. and Walt M.  Phys.rev.117,1330 (1960)               525 1451  203'
         open(filename, 'w').write('\r\n'.join(text))
 
-    print('Converting: ' + filename)
+    print('Converting: ' + str(filename))
     data = openmc.data.IncidentNeutron.from_njoy(filename)
 
     # Export HDF5 file
-    h5_file = os.path.join(args.destination, data.name + '.h5')
+    h5_file = args.destination / f'{data.name}.h5'
     print('Writing {}...'.format(h5_file))
     data.export_to_hdf5(h5_file, 'w', libver=args.libver)
 
@@ -131,5 +128,4 @@ for filename in sorted(neutron_files):
     library.register_file(h5_file)
 
 # Write cross_sections.xml
-libpath = os.path.join(args.destination, 'cross_sections.xml')
-library.export_to_xml(libpath)
+library.export_to_xml(args.destination / 'cross_sections.xml')
