0cb064b2f5b7c5ea91da59944fa94ee6d75e72a4
Tim Fry <tim@frys.me.uk>
2020-02-10 14:01:47 +0000

    Update generate_jendl.py
    Use f-strings for comments
    
    Co-Authored-By: Paul Romano <paul.k.romano@gmail.com>

--- generate_jendl.py
+++ generate_jendl.py
@@ -108,7 +108,7 @@ library = openmc.data.DataLibrary()
 
 for filename in sorted(neutron_files):
 
-    print('Converting: ' + str(filename))
+    print(f'Converting: {filename}')
     data = openmc.data.IncidentNeutron.from_njoy(filename)
 
     # Export HDF5 file
