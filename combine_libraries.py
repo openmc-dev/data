d25df554fed9630563902127fe05e97018c66505
Tim Fry <tim@frys.me.uk>
2020-02-13 12:27:21 +0000

    Update combine_libraries.py
    Cleanup, use absolute paths in log/error messages and add more error handling.

--- combine_libraries.py
+++ combine_libraries.py
@@ -38,28 +38,38 @@ parser.add_argument('-d', '--destination', type=Path,
                     help='Directory to create new library in', required=True)
 parser.add_argument('-l', '--libraries', type=Path,
                     help='List of data libraries to create', required=True, nargs='+')
-parser.add_argument('-n', '--nocopy', action='store_true',
+parser.add_argument('-n', '--no-copy', dest='copy', action='store_true',
                     help='Don\'t copy library files, just create a cross_sections.xml file')
 args = parser.parse_args()
 
 read_libraries = []
 
+# Error check arguments
+if args.destination.exists():
+    if not args.destination.is_dir():
+        print(f'Error: Destination {args.destination.resolve()} should be a directory')
+        sys.exit()
+    if len(list(args.destination.glob('*'))) > 0:
+        print(f'Error: Destination {args.destination.resolve()} is not empty')
+        sys.exit()
+
 # Parse cross_sections.xml from each library
 for lib_dir in args.libraries:
     lib_cross_sections_file = lib_dir / 'cross_sections.xml'
     if not lib_cross_sections_file.exists():
-        print(f'Error: Unable to find cross_sections.xml file in {lib_dir}')
+        print(f'Error: Unable to find cross_sections.xml file in {lib_dir.resolve()}')
         sys.exit()
     parsed_library = openmc.data.DataLibrary.from_xml(lib_cross_sections_file)
     read_libraries.append(parsed_library)
 
 i = 1
-print(f'Creating library in {args.destination}'
+print(f'Creating library in {args.destination.resolve()}'
       + ' from the following nuclides in order or preference:')
 for lib_dir in args.libraries:
-    print(f'{i}: {lib_dir}')
+    print(f'{i}) {lib_dir.resolve()}')
     i += 1
-# TODO print nocopy message
+if args.copy:
+    print('Original library files will not be copied into the destination folder')
 
 # Create output directory if it doesn't exist
 args.destination.mkdir(parents=True, exist_ok=True)
@@ -71,10 +81,10 @@ for library in read_libraries[0].libraries:
     source_file = Path(library['path'])
     destination_file = source_file
     source_path = source_file.relative_to(args.libraries[0])
-    if not args.nocopy:
+    if not args.copy:
         destination_file = args.destination / source_file.name
         shutil.copy(source_file, args.destination)
-    print(f'Adding {source_path} from {args.libraries[0]}')
+    print(f'Adding {source_path} from {args.libraries[0].resolve()}')
     combined_library.register_file(destination_file)
 
 # For each other libraries, check library and add if not already present
@@ -84,11 +94,14 @@ for lib_num in range(1, len(read_libraries)):
             source_file = Path(library['path'])
             destination_file = source_file
             source_path = source_file.relative_to(args.libraries[lib_num])
-            if not args.nocopy:
+            if not args.copy:
                 destination_file = args.destination / source_file.name
-                # TODO Check destination doesn't already exist
+                if destination_file.exists():
+                    print(f'Error: Library file {destination_file.name} already'
+                          + ' exists in the combined library')
+                    sys.exit()
                 shutil.copy(source_file, args.destination)
-            print(f'Adding {source_path} from {args.libraries[lib_num]}')
+            print(f'Adding {source_path} from {args.libraries[lib_num].resolve()}')
             combined_library.register_file(destination_file)
 
 # Write cross_sections.xml
