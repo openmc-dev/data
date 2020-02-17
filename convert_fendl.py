f2013fa273cfc31d92d04e1b3b5d475297a38317
Sam Powell-Gill <sampg95@gmail.com>
2020-02-17 09:05:29 +0000

    Initial commit adding 2.1 release

--- convert_fendl.py
+++ convert_fendl.py
@@ -8,6 +8,7 @@ import sys
 import zipfile
 from urllib.parse import urljoin
 from textwrap import dedent
+from os import chdir
 
 import openmc.data
 from openmc._utils import download
@@ -42,16 +43,20 @@ parser.add_argument('--libver', choices=['earliest', 'latest'],
                     default='earliest', help="Output HDF5 versioning. Use "
                     "'earliest' for backwards compatibility or 'latest' for "
                     "performance")
-parser.add_argument('-r', '--release', choices=['3.1a', '3.1d', '3.0'],
+parser.add_argument('-r', '--release', choices=['3.1a', '3.1d', '3.0', '2.1'],
                     default='3.1d', help="The nuclear data library release version. "
-                    "The currently supported options are 3.1a and 3.1d")
+                    "The currently supported options are 3.1d, 3.1a, 3.0 and "
+                    "2.1")
 parser.set_defaults(download=True, extract=True)
 args = parser.parse_args()
 
 
 # this could be added as an argument to allow different libraries to be downloaded
 library_name = 'fendl'
-ace_files_dir = Path('-'.join([library_name, args.release, 'ace']))
+cwd = Path.cwd()  
+
+ace_files_dir = cwd.joinpath('-'.join([library_name, args.release, 'ace']))
+zip_path = cwd.joinpath('-'.join([library_name, args.release, 'zips']))
 # the destination is decided after the release is know to avoid putting the release in a folder with a misleading name
 if args.destination is None:
     args.destination = Path('-'.join([library_name, args.release, 'hdf5']))
@@ -78,6 +83,30 @@ release_details = {
         'neutron_files': ace_files_dir.joinpath('ace').glob('*.ace'),
         'compressed_file_size': '0.4 GB',
         'uncompressed_file_size': '2.2 GB'
+    },
+    '2.1': {
+        'base_url': 'https://www-nds.iaea.org/fendl21/fendl21mc/',
+        'files': ['H001mc.zip',  'H002mc.zip',  'H003mc.zip',  'He003mc.zip', 
+                  'He004mc.zip', 'Li006mc.zip', 'Li007mc.zip', 'Be009mc.zip',
+                  'B010mc.zip',  'B011mc.zip',  'C012mc.zip',  'N014mc.zip', 
+                  'N015mc.zip',  'O016mc.zip',  'F019mc.zip',  'Na023mc.zip',
+                  'Mg000mc.zip', 'Al027mc.zip', 'Si028mc.zip', 'Si029mc.zip',
+                  'Si030mc.zip', 'P031mc.zip',  'S000mc.zip',  'Cl035mc.zip', 
+                  'Cl037mc.zip', 'K000mc.zip',  'Ca000mc.zip', 'Ti046mc.zip', 
+                  'Ti047mc.zip', 'Ti048mc.zip', 'Ti049mc.zip', 'Ti050mc.zip', 
+                  'V000mc.zip',  'Cr050mc.zip', 'Cr052mc.zip', 'Cr053mc.zip', 
+                  'Cr054mc.zip', 'Mn055mc.zip', 'Fe054mc.zip', 'Fe056mc.zip', 
+                  'Fe057mc.zip', 'Fe058mc.zip', 'Co059mc.zip', 'Ni058mc.zip', 
+                  'Ni060mc.zip', 'Ni061mc.zip', 'Ni062mc.zip', 'Ni064mc.zip', 
+                  'Cu063mc.zip', 'Cu065mc.zip', 'Ga000mc.zip', 'Zr000mc.zip', 
+                  'Nb093mc.zip', 'Mo092mc.zip', 'Mo094mc.zip', 'Mo095mc.zip', 
+                  'Mo096mc.zip', 'Mo097mc.zip', 'Mo098mc.zip', 'Mo100mc.zip', 
+                  'Sn000mc.zip', 'Ta181mc.zip', 'W182mc.zip',  'W183mc.zip',  
+                  'W184mc.zip',  'W186mc.zip',  'Au197mc.zip', 'Pb206mc.zip', 
+                  'Pb207mc.zip', 'Pb208mc.zip', 'Bi209mc.zip'],
+        'neutron_files': ace_files_dir.glob('*.ace'),
+        'compressed_file_size': '0.4 GB',
+        'uncompressed_file_size': '2.2 GB'
     }
 }
 
@@ -92,19 +121,31 @@ Extracting and processing the data requires {} of additional free disk space.
 
 if args.download:
     print(download_warning)
+
+    if args.release == '2.1':
+        # Older releases have ace files in individual zip files. Create a 
+        # a directory to hold them.
+        zip_path.mkdir(exist_ok=True) 
+        chdir(zip_path)
+
     for f in release_details[args.release]['files']:
         download(urljoin(release_details[args.release]['base_url'], f),
                     as_browser=True, context=ssl._create_unverified_context())
+    chdir(cwd)
 
 # ==============================================================================
 # EXTRACT FILES FROM TGZ
 if args.extract:
+    if args.release == '2.1':
+        chdir(zip_path)
+
     for f in release_details[args.release]['files']:
         # Extract files, the fendl release was compressed using type 9 zip format
         # unfortunatly which is incompatible with the standard python zipfile library
         # therefore the following system command is used
 
         subprocess.call(['unzip', '-o', f, '-d', ace_files_dir])
+    chdir(cwd)
 
 # ==============================================================================
 # GENERATE HDF5 LIBRARY -- NEUTRON FILES
@@ -121,7 +162,7 @@ args.destination.mkdir(parents=True, exist_ok=True)
 
 library = openmc.data.DataLibrary()
 
-warn_k39 = False
+warn_k39 = False 
 
 for filename in sorted(neutron_files):
 
