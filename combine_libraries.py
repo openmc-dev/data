fc8cb3bd977675837fab0ad1f0d5987f11be7c3f
Tim Fry <tim@frys.me.uk>
2020-03-12 15:06:41 +0000

    Update script arguments and associated error checks

--- combine_libraries.py
+++ combine_libraries.py
@@ -35,23 +35,30 @@ parser = argparse.ArgumentParser(
     formatter_class=CustomFormatter
 )
 parser.add_argument('-d', '--destination', type=Path,
-                    help='Directory to create new library in', required=True)
+                    help='Directory to create new library in')
 parser.add_argument('-l', '--libraries', type=Path,
-                    help='List of data libraries to create', required=True, nargs='+')
-parser.add_argument('-n', '--no-copy', dest='copy', action='store_true',
+                    help='List of data libraries to combine', nargs='+')
+parser.add_argument('--copy', action='store_true',
+                    help="Copy library files and create a cross_sections.xml file")
+parser.add_argument('--no-copy', dest='copy', action='store_false',
                     help="Don't copy library files, just create a cross_sections.xml file")
+parser.set_defaults(copy=True)
 args = parser.parse_args()
 
 read_libraries = []
 
 # Error check arguments
+if args.destination is None:
+    raise OSError('Destination folder not specified')
 if args.destination.exists():
     if not args.destination.is_dir():
         raise NotADirectoryError(f'Destination {args.destination.resolve()} should be a directory')
-    if not any(args.destination.iterdir()):
+    if any(args.destination.iterdir()):
         raise OSError(f'Destination {args.destination.resolve()} is not empty')
 
 # Parse cross_sections.xml from each library
+if args.libraries is None:
+    raise OSError('No input libraries specified')
 for lib_dir in args.libraries:
     lib_cross_sections_file = lib_dir / 'cross_sections.xml'
     if not lib_cross_sections_file.exists():
@@ -63,7 +70,7 @@ print(f'Creating library in {args.destination.resolve()}'
       ' from the following nuclides in order of preference:')
 for i, lib_dir in enumerate(args.libraries):
     print(f'{i + 1}) {lib_dir.resolve()}')
-if args.copy:
+if not args.copy:
     print('Original library files will not be copied into the destination folder')
 
 # Create output directory if it doesn't exist
@@ -76,7 +83,7 @@ for library in read_libraries[0].libraries:
     source_file = Path(library['path'])
     destination_file = source_file
     source_path = source_file.relative_to(args.libraries[0])
-    if not args.copy:
+    if args.copy:
         destination_file = args.destination / source_file.name
         shutil.copy(source_file, args.destination)
     print(f'Adding {source_path} from {args.libraries[0].resolve()}')
@@ -89,7 +96,7 @@ for lib_num in range(1, len(read_libraries)):
             source_file = Path(library['path'])
             destination_file = source_file
             source_path = source_file.relative_to(args.libraries[lib_num])
-            if not args.copy:
+            if args.copy:
                 destination_file = args.destination / source_file.name
                 if destination_file.exists():
                     raise FileExistsError(f'Library file {destination_file.name} already'
