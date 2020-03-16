85309909a91073839d2c7ef5e0ad35042951a404
=shimwell <mail@jshimwell.com>
2020-03-16 15:44:54 +0000

    recombined wmp and endf

--- generate_endf71.py
+++ generate_endf71.py
@@ -8,11 +8,12 @@ used for OpenMC's regression test suite.
 
 import argparse
 import sys
+import tarfile
 import warnings
 import zipfile
 from multiprocessing import Pool
 from pathlib import Path
-from shutil import rmtree
+from shutil import rmtree, copy
 
 import openmc.data
 from utils import download
@@ -46,8 +47,8 @@ parser.add_argument('--libver', choices=['earliest', 'latest'],
                     default='earliest', help="Output HDF5 versioning. Use "
                     "'earliest' for backwards compatibility or 'latest' for "
                     "performance")
-parser.add_argument('-p', '--particles', choices=['neutron', 'photon'],
-                    nargs='+', default=['neutron', 'photon'],
+parser.add_argument('-p', '--particles', choices=['neutron', 'photon', 'neutron_wmp'],
+                    nargs='+', default=['neutron', 'photon', 'neutron_wmp'],
                     help="Incident particles to include")
 parser.add_argument('--cleanup', action='store_true',
                     help="Remove download directories when data has "
@@ -104,8 +105,9 @@ release = 'b7.1'
 
 cwd = Path.cwd()
 
-# ace_files_dir = Path('-'.join([library_name, release, 'ace']))
-endf_files_dir = Path('-'.join([library_name, release, 'endf']))
+wmp_files_dir = args.destination / 'neutron_wmp'
+# wmp_files_dir = cwd.joinpath('-'.join([library_name, release, 'wmp']))
+endf_files_dir = cwd.joinpath('-'.join([library_name, release, 'endf']))
 download_path = cwd.joinpath('-'.join([library_name, release, 'download']))
 
 temperatures = [250.0, 293.6, 600.0, 900.0, 1200.0, 2500.0]
@@ -159,14 +161,22 @@ release_details = {
             'atom_files': endf_files_dir.joinpath('atomic_relax').rglob('*.endf'),
             'compressed_file_size': 9,
             'uncompressed_file_size': 45
+        },
+        'neutron_wmp': {
+            'base_url': 'https://github.com/mit-crpg/WMP_Library/releases/download/v1.1/',
+            'compressed_files': ['WMP_Library_v1.1.tar.gz'],
+            'file_type': 'wmp',
+            'compressed_file_size': 12,
+            'uncompressed_file_size': 17
         }
     }
 }
 
 compressed_file_size, uncompressed_file_size = 0, 0
-for p in ('neutron', 'photon'):
-    compressed_file_size += release_details[release][p]['compressed_file_size']
-    uncompressed_file_size += release_details[release][p]['uncompressed_file_size']
+for r in release:
+    for p in ('neutron', 'photon', 'neutron_wmp'):
+        compressed_file_size += release_details[release][p]['compressed_file_size']
+        uncompressed_file_size += release_details[release][p]['uncompressed_file_size']
 
 download_warning = """
 WARNING: This script will download up to {} MB of data. Extracting and
@@ -181,28 +191,47 @@ for use with OpenMC.
 # DOWNLOAD FILES FROM NNDC SITE
 
 if args.download:
-    # print(download_warning)
+    print(download_warning)
     for particle in args.particles:
-        for f, checksum in zip(release_details[release][particle]['compressed_files'],
-                               release_details[release][particle]['checksums']):
-            # Establish connection to URL
-            url = release_details[release][particle]['base_url'] + f
-            downloaded_file = download(url,
-                                       output_path=download_path / particle,
-                                       checksum=checksum)
-
+        details = release_details[release][particle]
+        for i, f in enumerate(details['compressed_files']):
+            url = details['base_url'] + f
+            if 'checksums' in details.keys():
+                checksum = details['checksums'][i]
+                downloaded_file = download(url,
+                                           output_path=download_path / particle,
+                                           checksum=checksum)
+            else:
+                downloaded_file = download(url,
+                                           output_path=download_path / particle,
+                                           )
 
 # ==============================================================================
 # EXTRACT FILES FROM TGZ
 
 if args.extract:
     for particle in args.particles:
+
+        if release_details[release][particle]['file_type'] == 'wmp':
+            extraction_dir = wmp_files_dir
+        elif release_details[release][particle]['file_type'] == 'endf':
+            extraction_dir = endf_files_dir
+
         for f in release_details[release][particle]['compressed_files']:
 
-            # Extract files
-            with zipfile.ZipFile(download_path / particle / Path(f), 'r') as zipf:
-                print('Extracting {}...'.format(f))
-                zipf.extractall(endf_files_dir / particle)
+            # Extract files different depending on compression method
+            if f.endswith('.zip'):
+                with zipfile.ZipFile(download_path / particle / f, 'r') as zipf:
+                    print(f'Extracting {f}...')
+                    zipf.extractall(extraction_dir)
+            else:
+                with tarfile.open(download_path / particle / Path(f), 'r') as tgz:
+                    print(f'Extracting {f}...')
+                    # extract files ignoring the internal folder structure
+                    for member in tgz.getmembers():
+                        if member.isreg(): 
+                            member.name = Path(member.name).name
+                            tgz.extract(member,path=extraction_dir)
 
     if args.cleanup and download_path.exists():
         rmtree(download_path)
@@ -218,30 +247,30 @@ for particle in args.particles:
 
 library = openmc.data.DataLibrary()
 
-
 if 'neutron' in args.particles:
+    particle = 'neutron'
     with Pool() as pool:
-        details = release_details[release]['neutron']
+        details = release_details[release][particle]
         results = []
         for filename in details['endf_files']:
 
             r = pool.apply_async(process_neutron,
                                 (filename,
-                                args.destination / 'neutron'))
+                                args.destination / particle))
             results.append(r)
 
         for path_neutron, path_thermal in details['sab_files']:
 
             r = pool.apply_async(process_thermal,
                                 (path_neutron, path_thermal,
-                                args.destination))
+                                args.destination / particle))
 
             results.append(r)
 
         for r in results:
             r.wait()
 
-    for p in sorted((args.destination / 'neutron').glob('*.h5'), key=sort_key):
+    for p in sorted((args.destination / particle).glob('*.h5'), key=sort_key):
         library.register_file(p)
 
 
@@ -249,6 +278,8 @@ if 'neutron' in args.particles:
 # INCIDENT PHOTON DATA
 
 if 'photon' in args.particles:
+    particle = 'photon'
+    details = release_details[release][particle]
     for photo_path, atom_path in zip(sorted(details['photo_files']),
                                      sorted(details['atom_files'])):
         # Generate instance of IncidentPhoton
@@ -260,6 +291,14 @@ if 'photon' in args.particles:
         data.export_to_hdf5(h5_file, 'w', libver=args.libver)
 
         # Register with library
+        library.register_file(h5_file)
+
+
+# =========================================================================
+# INCIDENT WMP NEUTRON DATA
+
+if 'neutron_wmp'in args.particles:
+    for h5_file in Path(wmp_files_dir).rglob('*.h5'):
         library.register_file(h5_file)
 
 # Write cross_sections.xml
