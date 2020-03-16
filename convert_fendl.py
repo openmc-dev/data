fadd1c03145499c0dfd2271122d4419fada33f8d
=shimwell <mail@jshimwell.com>
2020-03-16 10:58:17 +0000

    flake8 pep tidy up

--- convert_fendl.py
+++ convert_fendl.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
 """
-Download FENDL-3.1d, FENDL-3.1a, FENDL-3.0 or FENDL-2.1  ACE data from the IAEA 
+Download FENDL-3.1d, FENDL-3.1a, FENDL-3.0 or FENDL-2.1 ACE data from the IAEA
 and convert it to a HDF5 library for use with OpenMC.
 """
 
 import argparse
-from pathlib import Path
 import ssl
 import subprocess
-from urllib.parse import urljoin
-from textwrap import dedent
-import os
-from shutil import rmtree
 import warnings
+from pathlib import Path
+from shutil import rmtree
+from textwrap import dedent
+from urllib.parse import urljoin
 
 import openmc.data
 from utils import download
@@ -43,11 +42,11 @@ parser.add_argument('--libver', choices=['earliest', 'latest'],
                     "'earliest' for backwards compatibility or 'latest' for "
                     "performance")
 parser.add_argument('-r', '--release', choices=['3.1d', '3.1a', '3.0', '2.1'],
-                    default='3.1d', help="The nuclear data library release version. "
-                    "The currently supported options are 3.1d, 3.1a, 3.0 and "
-                    "2.1")
-parser.add_argument('-p', '--particles', choices=['neutron', 'photon'], 
-                    nargs='+', default=['neutron', 'photon'], 
+                    default='3.1d', help="The nuclear data library release "
+                    "version. The currently supported options are "
+                    "3.1d, 3.1a, 3.0 and 2.1")
+parser.add_argument('-p', '--particles', choices=['neutron', 'photon'],
+                    nargs='+', default=['neutron', 'photon'],
                     help="Incident particles to include")
 parser.add_argument('--cleanup', action='store_true',
                     help="Remove download directories when data has "
@@ -65,45 +64,50 @@ args = parser.parse_args()
 # The function should return a bool which determines whether the file should be
 # ignored.
 
-# Function to check for k-39 error in FENDL-3.0
+
 def fendl30_k39(file_path):
+    """ Function to check for k-39 error in FENDL-3.0"""
     if 'Inf' in open(file_path, 'r').read():
-        ace_error_warning = """ 
-        {} contains 'Inf' values within the XSS array 
+        ace_error_warning = """
+        {} contains 'Inf' values within the XSS array
         which prevent conversion to a HDF5 file format. This is a known issue
-        in FENDL-3.0. {} has not been added to the cross section library. 
+        in FENDL-3.0. {} has not been added to the cross section library.
         """.format(file_path, file_path.name)
         err_msg = dedent(ace_error_warning)
         return {'skip_file': True, 'err_msg': err_msg}
     else:
         return {'skip_file': False}
 
-# Helper function for checking if there are any special cases defined:
-# Returns the special Cases relevant to a specific part of the script. 
-# If there are no special cases, return an empty dict
+
 def check_special_case(particle_details, script_step):
+    """
+    Helper function for checking if there are any special cases defined:
+    Returns the special Cases relevant to a specific part of the script.
+    If there are no special cases, return an empty dict
+    """
     if 'special_cases' in particle_details:
         if script_step in particle_details['special_cases']:
             return particle_details['special_cases'][script_step]
     return {}
 
-# this could be added as an argument to allow different libraries to be downloaded
+
 library_name = 'fendl'
-cwd = Path.cwd()  
+cwd = Path.cwd()
 
 ace_files_dir = cwd.joinpath('-'.join([library_name, args.release, 'ace']))
 endf_files_dir = cwd.joinpath('-'.join([library_name, args.release, 'endf']))
 
 download_path = cwd.joinpath('-'.join([library_name, args.release, 'download']))
-# the destination is decided after the release is know to avoid putting the release in a folder with a misleading name
+# the destination is decided after the release is know to avoid putting
+# the release in a folder with a misleading name
 if args.destination is None:
     args.destination = Path('-'.join([library_name, args.release, 'hdf5']))
 
-# This dictionary contains all the unique information about each release. 
+# This dictionary contains all the unique information about each release.
 # This can be extended to accommodate new releases
 release_details = {
     '3.1a': {
-        'neutron':{
+        'neutron': {
             'base_url': 'https://www-nds.iaea.org/fendl31/data/neutron/',
             'compressed_files': ['fendl31a-neutron-ace.zip'],
             'file_type': 'ace',
@@ -111,7 +115,7 @@ release_details = {
             'compressed_file_size': 384,
             'uncompressed_file_size': 2250
         },
-        'photon':{
+        'photon': {
             'base_url': 'https://www-nds.iaea.org/fendl31/data/atom/',
             'compressed_files': ['fendl30-atom-endf.zip'],
             'file_type': 'endf',
@@ -121,7 +125,7 @@ release_details = {
         }
     },
     '3.1d': {
-        'neutron':{
+        'neutron': {
             'base_url': 'https://www-nds.iaea.org/fendl/data/neutron/',
             'compressed_files': ['fendl31d-neutron-ace.zip'],
             'file_type': 'ace',
@@ -129,7 +133,7 @@ release_details = {
             'compressed_file_size': 425,
             'uncompressed_file_size': 2290
         },
-        'photon':{
+        'photon': {
             'base_url': 'https://www-nds.iaea.org/fendl/data/atom/',
             'compressed_files': ['fendl30-atom-endf.zip'],
             'file_type': 'endf',
@@ -139,7 +143,7 @@ release_details = {
         }
     },
     '3.0': {
-        'neutron':{
+        'neutron': {
             'base_url': 'https://www-nds.iaea.org/fendl30/data/neutron/',
             'compressed_files': ['fendl30-neutron-ace.zip'],
             'file_type': 'ace',
@@ -147,10 +151,10 @@ release_details = {
             'compressed_file_size': 364,
             'uncompressed_file_size': 2200,
             'special_cases': {
-                'process': {'19K_039.ace':fendl30_k39}
+                'process': {'19K_039.ace': fendl30_k39}
             }
         },
-        'photon':{
+        'photon': {
             'base_url': 'https://www-nds.iaea.org/fendl30/data/atom/',
             'compressed_files': ['fendl30-atom-endf.zip'],
             'file_type': 'endf',
@@ -160,32 +164,32 @@ release_details = {
         }
     },
     '2.1': {
-        'neutron':{
+        'neutron': {
             'base_url': 'https://www-nds.iaea.org/fendl21/fendl21mc/',
-            'compressed_files': ['H001mc.zip',  'H002mc.zip',  'H003mc.zip',  'He003mc.zip', 
+            'compressed_files': ['H001mc.zip',  'H002mc.zip',  'H003mc.zip',  'He003mc.zip',
                     'He004mc.zip', 'Li006mc.zip', 'Li007mc.zip', 'Be009mc.zip',
-                    'B010mc.zip',  'B011mc.zip',  'C012mc.zip',  'N014mc.zip', 
+                    'B010mc.zip',  'B011mc.zip',  'C012mc.zip',  'N014mc.zip',
                     'N015mc.zip',  'O016mc.zip',  'F019mc.zip',  'Na023mc.zip',
                     'Mg000mc.zip', 'Al027mc.zip', 'Si028mc.zip', 'Si029mc.zip',
-                    'Si030mc.zip', 'P031mc.zip',  'S000mc.zip',  'Cl035mc.zip', 
-                    'Cl037mc.zip', 'K000mc.zip',  'Ca000mc.zip', 'Ti046mc.zip', 
-                    'Ti047mc.zip', 'Ti048mc.zip', 'Ti049mc.zip', 'Ti050mc.zip', 
-                    'V000mc.zip',  'Cr050mc.zip', 'Cr052mc.zip', 'Cr053mc.zip', 
-                    'Cr054mc.zip', 'Mn055mc.zip', 'Fe054mc.zip', 'Fe056mc.zip', 
-                    'Fe057mc.zip', 'Fe058mc.zip', 'Co059mc.zip', 'Ni058mc.zip', 
-                    'Ni060mc.zip', 'Ni061mc.zip', 'Ni062mc.zip', 'Ni064mc.zip', 
-                    'Cu063mc.zip', 'Cu065mc.zip', 'Ga000mc.zip', 'Zr000mc.zip', 
-                    'Nb093mc.zip', 'Mo092mc.zip', 'Mo094mc.zip', 'Mo095mc.zip', 
-                    'Mo096mc.zip', 'Mo097mc.zip', 'Mo098mc.zip', 'Mo100mc.zip', 
-                    'Sn000mc.zip', 'Ta181mc.zip', 'W182mc.zip',  'W183mc.zip',  
-                    'W184mc.zip',  'W186mc.zip',  'Au197mc.zip', 'Pb206mc.zip', 
+                    'Si030mc.zip', 'P031mc.zip',  'S000mc.zip',  'Cl035mc.zip',
+                    'Cl037mc.zip', 'K000mc.zip',  'Ca000mc.zip', 'Ti046mc.zip',
+                    'Ti047mc.zip', 'Ti048mc.zip', 'Ti049mc.zip', 'Ti050mc.zip',
+                    'V000mc.zip',  'Cr050mc.zip', 'Cr052mc.zip', 'Cr053mc.zip',
+                    'Cr054mc.zip', 'Mn055mc.zip', 'Fe054mc.zip', 'Fe056mc.zip',
+                    'Fe057mc.zip', 'Fe058mc.zip', 'Co059mc.zip', 'Ni058mc.zip',
+                    'Ni060mc.zip', 'Ni061mc.zip', 'Ni062mc.zip', 'Ni064mc.zip',
+                    'Cu063mc.zip', 'Cu065mc.zip', 'Ga000mc.zip', 'Zr000mc.zip',
+                    'Nb093mc.zip', 'Mo092mc.zip', 'Mo094mc.zip', 'Mo095mc.zip',
+                    'Mo096mc.zip', 'Mo097mc.zip', 'Mo098mc.zip', 'Mo100mc.zip',
+                    'Sn000mc.zip', 'Ta181mc.zip', 'W182mc.zip',  'W183mc.zip',
+                    'W184mc.zip',  'W186mc.zip',  'Au197mc.zip', 'Pb206mc.zip',
                     'Pb207mc.zip', 'Pb208mc.zip', 'Bi209mc.zip'],
             'file_type': 'ace',
             'ace_files': ace_files_dir.glob('*.ace'),
             'compressed_file_size': 100,
             'uncompressed_file_size': 600
         },
-        'photon':{
+        'photon': {
             'base_url': 'https://www-nds.iaea.org/fendl21/fendl21e/',
             'compressed_files': ['FENDLEP.zip'],
             'file_type': 'endf',
@@ -208,7 +212,7 @@ Extracting and processing the data requires {} MB of additional free disk space.
 """.format(compressed_file_size, uncompressed_file_size)
 
 # Warnings to be printed at the end of the script.
-output_warnings = [] 
+output_warnings = []
 
 # ==============================================================================
 # DOWNLOAD FILES FROM IAEA SITE
@@ -269,12 +273,12 @@ for particle in args.particles:
     particle_destination.mkdir(parents=True, exist_ok=True)
 
     particle_details = release_details[args.release][particle]
-    
+
     # Get dictionary of special cases for particle
     special_cases = check_special_case(particle_details, 'process')
 
     if particle == 'neutron':
-        # Get a list of all ACE files, excluding files ending with _ which are 
+        # Get a list of all ACE files, excluding files ending with which are
         # old incorrect files kept in the release for backwards compatability
         neutron_files = [
             f
@@ -290,7 +294,7 @@ for particle in args.particles:
                     output_warnings.append(ret['err_msg'])
                 if ret['skip_file']:
                     continue
-            
+
             print(f'Converting: {filename}')
             data = openmc.data.IncidentNeutron.from_ace(filename)
 
@@ -302,7 +306,7 @@ for particle in args.particles:
             # Register with library
             library.register_file(h5_file)
 
-        # Remove the ace files if required    
+        # Remove the ace files if required
         if args.cleanup and ace_files_dir.exists():
             rmtree(ace_files_dir)
 
@@ -315,7 +319,7 @@ for particle in args.particles:
                     output_warnings.append(ret['err_msg'])
                 if ret['skip_file']:
                     continue
-            
+
             print(f'Converting: {photo_path}')
             evaluations = openmc.data.endf.get_evaluations(photo_path)
             for ev in evaluations:
@@ -327,8 +331,8 @@ for particle in args.particles:
 
             # Register with library
             library.register_file(h5_file)
-        
-        # Remove the ENDF files if required 
+
+        # Remove the ENDF files if required
         if args.cleanup and endf_files_dir.exists():
             rmtree(endf_files_dir)
 
