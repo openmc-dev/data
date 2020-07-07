469fd12f3dee93d6ef8417733197c71fdc878caf
Ander Gray <ander.gray@liverpool.ac.uk>
2020-07-07 19:32:50 +0100

    adds pathlib and suggested format

--- convert_tendl_rand.py
+++ convert_tendl_rand.py
@@ -5,181 +5,203 @@ import glob
 import os
 import sys
 import tarfile
+from pathlib import Path
 
 from multiprocessing import Pool
 
 import openmc.data
 from utils import download
 
-description = '''
+description = """
 Download random TENDL libraries from PSI and convert it to a HDF5 library for use with OpenMC. 
 Only certain nuclides are available from PSI. This script generates a cross_sections_Tendl.xml 
 file with random TENDL evaluations plus a standard library located in 'OPENMC_CROSS_SECTIONS'
-'''
+"""
 
 
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
-                      argparse.RawDescriptionHelpFormatter):
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter
+):
     pass
 
 
-n_choices=['all', 'O16','Si28', 'Si29','Si30', 'Fe54', 'Fe56', 'Fe57', 'Fe58', 'Na23', 'Pu240']
+n_choices = [
+    "all",
+    "O16",
+    "Si28",
+    "Si29",
+    "Si30",
+    "Fe54",
+    "Fe56",
+    "Fe57",
+    "Fe58",
+    "Na23",
+    "Pu240",
+]
 
 parser = argparse.ArgumentParser(
-    description=description,
-    formatter_class=CustomFormatter
+    description=description, formatter_class=CustomFormatter
 )
-parser.add_argument('-n', '--nuclides', choices=n_choices, nargs='+',
-                    default='Fe56', help="The nuclides to be downloaded. Available are: "
+parser.add_argument("-n", "--nuclides", choices=n_choices, nargs="+",
+                    default="Fe56", help="The nuclides to be downloaded. Available are: "
                     "'O16','Si28', 'Si29','Si30', 'Fe54', 'Fe56', 'Fe57', 'Fe58', 'Na23', 'Pu240'. Use 'all' for all availiable")
 
-parser.add_argument('-d', '--destination', default=None,
-                    help='Directory to create new library in')
+parser.add_argument( "-d", "--destination", default=None, 
+                    help="Directory to create new library in")
 
-parser.add_argument('-b', '--batch', action='store_true',		
-                     help='supresses standard in')
-
+parser.add_argument("-b", "--batch", action="store_true", 
+                    help="supresses standard in")
 
 args = parser.parse_args()
 
-if( 'all' in args.nuclides):
-    list_= ['O16','Si28', 'Si29','Si30', 'Fe54', 'Fe56', 'Fe57', 'Fe58', 'Na23', 'Pu240']
+if "all" in args.nuclides:
+    list_ = [
+        "O16",
+        "Si28",
+        "Si29",
+        "Si30",
+        "Fe54",
+        "Fe56",
+        "Fe57",
+        "Fe58",
+        "Na23",
+        "Pu240",
+    ]
 else:
     list_ = args.nuclides
-scriptDir = os.getcwd()
+scriptDir = Path.cwd()
 
-library_name = 'tendl_rand' #this could be added as an argument to allow different libraries to be downloaded
+library_name = "tendl_rand"  # this could be added as an argument to allow different libraries to be downloaded
 
 
 # the destination is decided after the release is know to avoid putting the release in a folder with a misleading name
 if args.destination is None:
-    outputDir = os.path.abspath(os.getcwd() + '/' + library_name)
+    outputDir = scriptDir / library_name
 else:
-    outputDir = os.path.abspath(args.destination)
+    outputDir = Path(os.path.abspath(args.destination))
 
 
-endf_files_dir  = '/'.join([outputDir, 'endf'])
-ace_files_dir   = '/'.join([outputDir, 'ace'])
-hdf5_files_dir  = '/'.join([outputDir, 'hdf5'])
+endf_files_dir = outputDir / "endf"
+ace_files_dir = outputDir / "ace"
+hdf5_files_dir = outputDir / "hdf5"
+
 
 release_details = {
-    'ENDF2017' : {
-        'base_url': 'https://tendl.web.psi.ch/tendl_2017/tar_files/',
-        'ending' : '.random.tgz',
-
+    "ENDF2017": {
+        "base_url": "https://tendl.web.psi.ch/tendl_2017/tar_files/",
+        "ending": ".random.tgz",
     },
-    'ENDF2015' : {
-        'base_url': 'https://tendl.web.psi.ch/tendl_2015/tar_files/',
-        'ending' : '.random.tgz',
+    "ENDF2015": {
+        "base_url": "https://tendl.web.psi.ch/tendl_2015/tar_files/",
+        "ending": ".random.tgz",
     },
-    'ACE2015' : {
-        'base_url': 'https://tendl.web.psi.ch/tendl_2015/tar_files/',
-        'ending' : '.10.2016.tgz',
-    }
+    "ACE2015": {
+        "base_url": "https://tendl.web.psi.ch/tendl_2015/tar_files/",
+        "ending": ".10.2016.tgz",
+    },
 }
 
 nuclide_details = {
-    'O16' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'O016',
-        'webname'  : 'O016',
-        'fileNum'  : 642,
-        'downSize' : 60,
-        'fileSize' : 244,
-        'isItENDF' : True,
-        'gunzip'   : False
+    "O16": {
+        "release": "ENDF2017",
+        "filename": "O016",
+        "webname": "O016",
+        "fileNum": 642,
+        "downSize": 60,
+        "fileSize": 244,
+        "isItENDF": True,
+        "gunzip": False,
     },
-    'Si28' : {        
-        'release'  : 'ENDF2017',
-        'filename' : 'Si028',
-        'webname'  : 'Si28',
-        'fileNum'  : 600,
-        'downSize' : 110,
-        'fileSize' : 428,
-        'isItENDF' : True,
-        'gunzip'   : True
+    "Si28": {
+        "release": "ENDF2017",
+        "filename": "Si028",
+        "webname": "Si28",
+        "fileNum": 600,
+        "downSize": 110,
+        "fileSize": 428,
+        "isItENDF": True,
+        "gunzip": True,
     },
-    'Si29' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Si029',
-        'webname'  : 'Si29',
-        'fileNum'  : 600,
-        'downSize' : 100,
-        'fileSize' : 425,
-        'isItENDF' : True,
-        'gunzip'   : True
+    "Si29": {
+        "release": "ENDF2017",
+        "filename": "Si029",
+        "webname": "Si29",
+        "fileNum": 600,
+        "downSize": 100,
+        "fileSize": 425,
+        "isItENDF": True,
+        "gunzip": True,
     },
-    'Si30' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Si030',
-        'webname'  : 'Si30',
-        'fileNum'  : 600,
-        'downSize' : 80,
-        'fileSize' : 337,
-        'isItENDF' : True,
-        'gunzip'   : True
+    "Si30": {
+        "release": "ENDF2017",
+        "filename": "Si030",
+        "webname": "Si30",
+        "fileNum": 600,
+        "downSize": 80,
+        "fileSize": 337,
+        "isItENDF": True,
+        "gunzip": True,
     },
-    'Fe54' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Fe054',
-        'webname'  : 'Fe054',
-        'fileNum'  : 501,
-        'downSize' : 320,
-        'fileSize' : 1300,
-        'isItENDF' : True,
-        'gunzip'   : False
+    "Fe54": {
+        "release": "ENDF2017",
+        "filename": "Fe054",
+        "webname": "Fe054",
+        "fileNum": 501,
+        "downSize": 320,
+        "fileSize": 1300,
+        "isItENDF": True,
+        "gunzip": False,
     },
-    'Fe56' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Fe056',
-        'webname'  : 'Fe056',
-        'fileNum'  : 614,
-        'downSize' : 410,
-        'fileSize' : 1600,
-        'isItENDF' : True,
-        'gunzip'   : False
-    }, 
-    'Fe57' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Fe057',
-        'webname'  : 'Fe057',
-        'fileNum'  : 733,
-        'downSize' : 525,
-        'fileSize' : 2000,
-        'isItENDF' : True,
-        'gunzip'   : False
-    }, 
-    'Fe58' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Fe058',
-        'webname'  : 'Fe058',
-        'fileNum'  : 501,
-        'downSize' : 280,
-        'fileSize' : 1100,
-        'isItENDF' : True,
-        'gunzip'   : False
+    "Fe56": {
+        "release": "ENDF2017",
+        "filename": "Fe056",
+        "webname": "Fe056",
+        "fileNum": 614,
+        "downSize": 410,
+        "fileSize": 1600,
+        "isItENDF": True,
+        "gunzip": False,
     },
-    'Na23' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Na023',
-        'webname'  : 'Na23',
-        'fileNum'  : 832,
-        'downSize' : 130,
-        'fileSize' : 544,
-        'isItENDF' : True,
-        'gunzip'   : False
+    "Fe57": {
+        "release": "ENDF2017",
+        "filename": "Fe057",
+        "webname": "Fe057",
+        "fileNum": 733,
+        "downSize": 525,
+        "fileSize": 2000,
+        "isItENDF": True,
+        "gunzip": False,
     },
-    'Pu240' : {
-        'release'  : 'ENDF2017',
-        'filename' : 'Pu240',
-        'webname'  : 'Pu240',
-        'fileNum'  : 630,
-        'downSize' : 200,
-        'fileSize' : 900,
-        'isItENDF' : True,
-        'gunzip'   : False
-    }
-
+    "Fe58": {
+        "release": "ENDF2017",
+        "filename": "Fe058",
+        "webname": "Fe058",
+        "fileNum": 501,
+        "downSize": 280,
+        "fileSize": 1100,
+        "isItENDF": True,
+        "gunzip": False,
+    },
+    "Na23": {
+        "release": "ENDF2017",
+        "filename": "Na023",
+        "webname": "Na23",
+        "fileNum": 832,
+        "downSize": 130,
+        "fileSize": 544,
+        "isItENDF": True,
+        "gunzip": False,
+    },
+    "Pu240": {
+        "release": "ENDF2017",
+        "filename": "Pu240",
+        "webname": "Pu240",
+        "fileNum": 630,
+        "downSize": 200,
+        "fileSize": 900,
+        "isItENDF": True,
+        "gunzip": False,
+    },
 }
 
 downloadFileSize = 0
@@ -187,16 +209,17 @@ uncompressedFileSize = 0
 NumOfFiles = 0
 
 for i in list_:
-    downloadFileSize += nuclide_details[i]['downSize']
-    uncompressedFileSize += nuclide_details[i]['fileSize']
-    NumOfFiles += nuclide_details[i]['fileNum']
+    downloadFileSize += nuclide_details[i]["downSize"]
+    uncompressedFileSize += nuclide_details[i]["fileSize"]
+    NumOfFiles += nuclide_details[i]["fileNum"]
 
-downloadSize = '{} MB'.format(downloadFileSize)
-uncomFileSize = '{} MB'.format(uncompressedFileSize)
-if (downloadFileSize > 1000): downloadSize = '{} GB'.format(downloadFileSize/1000)
-if (uncompressedFileSize > 1000): uncomFileSize = '{} GB'.format(uncompressedFileSize/1000)
+downloadSize = "{} MB".format(downloadFileSize)
+uncomFileSize = "{} MB".format(uncompressedFileSize)
+if downloadFileSize > 1000:
+    downloadSize = "{} GB".format(downloadFileSize / 1000)
+if uncompressedFileSize > 1000:
+    uncomFileSize = "{} GB".format(uncompressedFileSize / 1000)
 
-    
 
 download_warning = """
 WARNING: This script will download {} of 
@@ -207,12 +230,13 @@ The nuclides to be processed are:
 {}
 
 Are you sure you want to continue? ([y]/n)
-""".format(downloadSize, uncomFileSize,
-          NumOfFiles, list_)
+""".format(
+    downloadSize, uncomFileSize, NumOfFiles, list_
+)
 
 
-response = input(download_warning) if not args.batch else 'y'
-if response.lower().startswith('n'):
+response = input(download_warning) if not args.batch else "y"
+if response.lower().startswith("n"):
     sys.exit()
 
 # ==============================================================================
@@ -221,7 +245,11 @@ if response.lower().startswith('n'):
 files_complete = []
 for nucs in list_:
     # Establish connection to URL
-    url = release_details[nuclide_details[nucs]['release']]['base_url'] + nuclide_details[nucs]['webname'] + release_details[nuclide_details[nucs]['release']]['ending']
+    url = (
+        release_details[nuclide_details[nucs]["release"]]["base_url"]
+        + nuclide_details[nucs]["webname"]
+        + release_details[nuclide_details[nucs]["release"]]["ending"]
+    )
     print("Downloading {}...".format(nucs))
     downloaded_file = download(url)
 
@@ -229,102 +257,100 @@ for nucs in list_:
 # EXTRACT FILES FROM TGZ
 
 for nucs in list_:
-    f = nuclide_details[nucs]['webname'] + '.random.tgz'
+    f = nuclide_details[nucs]["webname"] + ".random.tgz"
     suffix = nucs
-    isItENDF = nuclide_details[nucs]['isItENDF']
+    isItENDF = nuclide_details[nucs]["isItENDF"]
     if isItENDF:
         outDir = endf_files_dir
     else:
         outDir = ace_files_dir
-    
-    with tarfile.open(f, 'r') as tgz:
-        print('Extracting {0}...'.format(f))
-        tgz.extractall(path=os.path.join(outDir, suffix))
-        
+
+    with tarfile.open(f, "r") as tgz:
+        print("Extracting {0}...".format(f))
+        tgz.extractall(path=outDir / suffix)
+
 # ==============================================================================
-# Format file names 
+# Format file names
 
 for nucs in list_:
-    f = nuclide_details[nucs]['webname'] + '.random.tgz'
-    isItENDF = nuclide_details[nucs]['isItENDF']
-    numFiles = nuclide_details[nucs]['fileNum']
+    f = nuclide_details[nucs]["webname"] + ".random.tgz"
+    isItENDF = nuclide_details[nucs]["isItENDF"]
+    numFiles = nuclide_details[nucs]["fileNum"]
+
     if isItENDF:
-        outDir = os.path.join(endf_files_dir, nucs)
-        prefix = 'n-'
-        suffix = '-rand-'
-        for i in range(0,numFiles):
-            OldNumber = f'{i:04}'
-            OldFile = prefix + nuclide_details[nucs]['filename']+suffix+OldNumber
-            newFile = nucs + '-' + str(i+1)
-            if nuclide_details[nucs]['gunzip']:
-                os.system('gunzip ' + os.path.join(outDir, OldFile) + '.gz')
-            os.rename(os.path.join(outDir, OldFile),os.path.join(outDir, newFile))
+        outDir = endf_files_dir / nucs
+        prefix = "n-"
+        suffix = "-rand-"
+
+        for i in range(0, numFiles):
+            OldNumber = f"{i:04}"
+            OldFile = prefix + nuclide_details[nucs]["filename"] + suffix + OldNumber
+            newFile = nucs + "-" + str(i + 1)
+            
+            if nuclide_details[nucs]["gunzip"]:
+                os.system("gunzip " + str(outDir) + OldFile + ".gz")
+            os.rename(os.path.join(outDir, OldFile), os.path.join(outDir, newFile))
     os.remove(f)
 
 # ==============================================================================
 # Convert ENDF files to HDF5 with njoy
 
-
 def process_neutron_random(nuc, i, outDir, inDir, fileNum):
     """Process ENDF neutron sublibrary file into HDF5 and write into a
     specified output directory."""
-    
-    fileIn  = inDir  + '/' + nuc + '-' + str(i)
-    fileOut = outDir + '/' + nuc + '-' + str(i) + '.h5'
 
-    data = openmc.data.IncidentNeutron.from_njoy(fileIn)#, temperatures=293.6)
-    data.name = nuc + '-' + str(i)
-    data.export_to_hdf5(fileOut, 'w')
-    if i % 40 == 0: 
-        print('Nuclide ' + nuc+ ' ' + str(i) + '/'+str(fileNum) + ' finished')
+    fileIn = inDir / (nuc + "-" + str(i))
+    fileOut = outDir / (nuc + "-" + str(i) + ".h5")
+
+    data = openmc.data.IncidentNeutron.from_njoy(fileIn)  # , temperatures=293.6)
+    data.name = nuc + "-" + str(i)
+    data.export_to_hdf5(fileOut, "w")
+    if i % 40 == 0:
+        print("Nuclide " + nuc + " " + str(i) + "/" + str(fileNum) + " finished")
 
 
-
-print('Beginning njoy processing')
+print("Beginning njoy processing")
 with Pool() as pool:
     results = []
     for nuc in list_:
 
-        fileNum = nuclide_details[nuc]['fileNum']
-        inDir  = endf_files_dir + '/' + nuc
-        outDir = hdf5_files_dir + '/' + nuc
+        fileNum = nuclide_details[nuc]["fileNum"]
+        inDir = endf_files_dir / nuc
+        outDir = hdf5_files_dir / nuc
 
-        if not os.path.exists(hdf5_files_dir):
-            os.mkdir(hdf5_files_dir)
-        if not os.path.exists(outDir):
-            os.mkdir(outDir)
+        hdf5_files_dir.mkdir(exist_ok=True)
+        outDir.mkdir(exist_ok=True)
 
-        print('Beginning nuclide '+ nuc + ' ...')
-        for i in range(1, fileNum+1):
-            func_args = (nuc,i, outDir, inDir, fileNum)
+        print("Beginning nuclide " + nuc + " ...")
+        for i in range(1, fileNum + 1):
+            func_args = (nuc, i, outDir, inDir, fileNum)
             r = pool.apply_async(process_neutron_random, func_args)
             results.append(r)
-        
+
     for r in results:
         r.wait()
 
 # ==============================================================================
 # Create xml library
 
-
-
 lib = openmc.data.DataLibrary()
-lib = lib.from_xml(os.getenv('OPENMC_CROSS_SECTIONS'))        #Gets current
+lib = lib.from_xml(os.getenv("OPENMC_CROSS_SECTIONS"))  # Gets current
 
 for nuc in list_:
-    fileNum = nuclide_details[nuc]['fileNum']
-    outDir = hdf5_files_dir + '/' + nuc
-    for i in range(1,fileNum+1):
-        fileOut = outDir + '/' + nuc + '-' + str(i) + '.h5'
-        lib.register_file(fileOut,nuc + '-' + str(i))
+    fileNum = nuclide_details[nuc]["fileNum"]
+    outDir = hdf5_files_dir / nuc
+    for i in range(1, fileNum + 1):
+        fileOut = outDir / (nuc + "-" + str(i) + ".h5")
+        lib.register_file(fileOut, nuc + "-" + str(i))
 
 
-pre = outputDir + '/cross_sections_PreT.xml'
-post = outputDir + '/cross_sections_Tendl.xml'
+pre = outputDir / "cross_sections_PreT.xml"
+post = outputDir / "cross_sections_Tendl.xml"
 
 lib.export_to_xml(pre)
 if os.path.exists(post):
-    command = "combine_libraries.py -l {} {} -o {}".format(pre, post, post)       # Error could not find ccombine_libs.py
+    command = "combine_libraries.py -l {} {} -o {}".format(
+        pre, post, post)
     os.system(command)
 else:
     lib.export_to_xml(post)
--- sample_sandy.py
+++ sample_sandy.py
@@ -8,168 +8,169 @@ import tarfile
 from multiprocessing import Pool
 import re
 import shutil
+from pathlib import Path
 
 import openmc.data
 
 
-description = '''
+description = """
 This scripts generates random (gaussian) evaluations of a nuclear data file following 
 its covariance matrix using SANDY, and converts them to HDF5 for use in OpenMC. Script
 generates a cross_sections.xml file with the standard library plus the sampled evaluations.
-'''
+"""
 
 
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
-                      argparse.RawDescriptionHelpFormatter):
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter
+):
     pass
 
 
 parser = argparse.ArgumentParser(
-    description=description,
-    formatter_class=CustomFormatter
-)
-parser.add_argument('-n', '--nuclides', nargs='+',
-                    default='Fe56', help="The nuclide(s) to be sampled")
+    description=description, formatter_class=CustomFormatter)
 
-parser.add_argument('-d', '--destination', default=None,
-                    help='Desitination of the endf library')
-
-parser.add_argument('-o', '--outdir', default=None,
-                    help='Directory to create new library in')
-
-parser.add_argument('-s', '--samples',  default=200,		
-                     help='Number of samples per nuclide')
-
-parser.add_argument('-p', '--processes',  default=1,		
-                     help='number of worker processes (default = 1)')
-
-parser.add_argument('-f', '--formatOnly',  default=False,		
-                     help='Only format previously sampled files to hdf5')
+parser.add_argument("-n", "--nuclides", nargs="+", 
+                    default="Fe56", help="The nuclide(s) to be sampled")
+parser.add_argument("-d", "--destination", default=None, 
+                    help="Desitination of the endf library")
+parser.add_argument("-o", "--outdir", default=None, 
+                    help="Directory to create new library in")
+parser.add_argument("-s", "--samples", default=200, 
+                    help="Number of samples per nuclide")
+parser.add_argument("-p", "--processes", default=1, 
+                    help="number of worker processes (default = 1)")
+parser.add_argument("-f", "--formatOnly", default=False,
+                    help="Only format previously sampled files to hdf5")
 
 args = parser.parse_args()
-scriptDir = os.getcwd()
+scriptDir = Path.cwd()
 
 outdir = args.outdir
 if outdir == None:
-    outdir = os.path.abspath(os.getcwd() + '/' + 'sandy_rand')
-else: 
-    outdir = os.path.abspath(outdir)
+    outdir = scriptDir / "sandy_rand"
+else:
+    outdir = Path(os.path.abspath(outdir))
 
-outdirEndf = outdir + '/' + 'endf'
-outdirHdf5 = outdir + '/' + 'hdf5'
+outdirEndf = outdir / "endf"
+outdirHdf5 = outdir / "hdf5"
 
 libdir = args.destination
 if libdir == None:
-    libdir = os.getenv('NUCLEAR_DATA_DIR') + '/nndc-b7.1-endf'
+    libdir = Path(os.getenv("NUCLEAR_DATA_DIR")) / "nndc-b7.1-endf"
 else:
-    libdir = os.path.abspath(libdir)
+    libdir = Path(os.path.abspath(libdir))
 
 nucs = args.nuclides
 
 # ==============================================================================
 # CHECK IF REQUEST IS VALID AND IF ENDF FILES EXIST
 
-prefix = 'n-'
-suffix = '.endf'
+prefix = "n-"
+suffix = ".endf"
 
 atomicDict = openmc.data.ATOMIC_NUMBER
 nucDict = {}
 
 for nuc in nucs:
-    massNum   = int(re.findall("(\d+)", nuc)[0])
-    atomicSym = ''.join([i for i in nuc if not i.isdigit()])
+    massNum = int(re.findall("(\d+)", nuc)[0])
+    atomicSym = "".join([i for i in nuc if not i.isdigit()])
     if atomicSym not in atomicDict.keys():
-        print('Entered nuclide {} does not have a valid atomic symbol'.format(nuc))
+        print("Entered nuclide {} does not have a valid atomic symbol".format(nuc))
         sys.exit()
     atomicNum = atomicDict[atomicSym]
 
-    fileMass   = f'{massNum:03}'
-    fileAtomic = f'{atomicNum:03}'
+    fileMass = f"{massNum:03}"
+    fileAtomic = f"{atomicNum:03}"
 
-    fileName = prefix + fileAtomic + '_' + atomicSym + '_' + fileMass + suffix
+    fileName = prefix + fileAtomic + "_" + atomicSym + "_" + fileMass + suffix
 
-    if not os.path.isfile(libdir+'/'+'neutron'+'/'+fileName):
-        print('File {} does not exist'.format(libdir+'/'+'neutron'+'/'+fileName))
+    if not os.path.isfile(libdir / "neutron" / fileName):
+        print("File {} does not exist".format(libdir / "neutron" / fileName))
         sys.exit()
-    nucDict[nuc] = {'sym' : atomicSym, 'massNum': massNum, 'atomicNum' : atomicNum, 'fileName': fileName}
+    nucDict[nuc] = {
+        "sym": atomicSym,
+        "massNum": massNum,
+        "atomicNum": atomicNum,
+        "fileName": fileName,
+    }
 
 # ==============================================================================
 # GENERATE RANDOM EVALUATIONS OF NUCLEAR DATA USING SANDY
 
-if not os.path.exists(outdir):
-    os.mkdir(outdir)
-
-if not os.path.exists(outdirEndf):
-    os.mkdir(outdirEndf)
+outdir.mkdir(exist_ok=True)
+outdirEndf.mkdir(exist_ok=True)
 
 for nuc in nucs:
-    nucDirEndf = outdirEndf + '/' + nuc
-    
-    if not os.path.exists(nucDirEndf):
-        os.mkdir(nucDirEndf)
 
-    shutil.copyfile(libdir+'/'+'neutron'+'/'+nucDict[nuc]['fileName'], nucDirEndf  + '/'+nucDict[nuc]['fileName'])
-    os.chdir(nucDirEndf+'/')
-    sandyCommand = 'sandy {} --samples {} --outname {} --processes {}'.format(nucDict[nuc]['fileName'], args.samples, nuc, args.processes)
+    nucDirEndf = outdirEndf / nuc
+    nucDirEndf.mkdir(exist_ok=True)
+
+    shutil.copyfile(
+        libdir / "neutron" / nucDict[nuc]["fileName"],
+        nucDirEndf / nucDict[nuc]["fileName"],
+    )
+    os.chdir(nucDirEndf)
+    sandyCommand = "sandy {} --samples {} --outname {} --processes {}".format(
+        nucDict[nuc]["fileName"], args.samples, nuc, args.processes
+    )
     os.system(sandyCommand)
 
 
 # ==============================================================================
 # CONVERT RANDOM EVALUATIONS TO HDF5
 
-def process_neutron_random(nuc, i, outDir, inDir, fileNum): # Need to add temperatures
+
+def process_neutron_random(nuc, i, outDir, inDir, fileNum):  # Need to add temperatures
     """Process ENDF neutron sublibrary file into HDF5 and write into a
     specified output directory."""
-    
-    fileIn  = inDir  + '/' + nuc + '-' + str(i)
-    fileOut = outDir + '/' + nuc + '-' + str(i) + '.h5'
 
-    data = openmc.data.IncidentNeutron.from_njoy(fileIn)#, temperatures=293.6)
-    data.name = nuc + '-' + str(i)
-    data.export_to_hdf5(fileOut, 'w')
-    if i % 40 == 0: 
-        print('Nuclide ' + nuc+ ' ' + str(i+1) + '/'+str(fileNum) + ' finished')
+    fileIn = inDir / (nuc + "-" + str(i))
+    fileOut = outDir / (nuc + "-" + str(i) + ".h5")
 
-print('Beginning njoy processing')
+    data = openmc.data.IncidentNeutron.from_njoy(fileIn)  # , temperatures=293.6)
+    data.name = nuc + "-" + str(i)
+    data.export_to_hdf5(fileOut, "w")
+    if i % 40 == 0:
+        print("Nuclide " + nuc + " " + str(i + 1) + "/" + str(fileNum) + " finished")
+
+
+print("Beginning njoy processing")
 with Pool() as pool:
     results = []
     fileNum = int(args.samples)
     for nuc in nucs:
 
-        inDir  = outdirEndf + '/' + nuc
-        outDir = outdirHdf5 + '/' + nuc
+        inDir = outdirEndf / nuc
+        outDir = outdirHdf5 / nuc
 
-        if not os.path.exists(outdirHdf5):
-            os.mkdir(outdirHdf5)
-        if not os.path.exists(outDir):
-            os.mkdir(outDir)
+        outdirHdf5.mkdir(exist_ok=True)
+        outDir.mkdir(exist_ok=True)
 
-        print('Beginning nuclide '+ nuc + ' ...')
-        for i in range(1, fileNum+1):
-            func_args = (nuc,i, outDir, inDir, fileNum)
+        print("Beginning nuclide " + nuc + " ...")
+        for i in range(1, fileNum + 1):
+            func_args = (nuc, i, outDir, inDir, fileNum)
             r = pool.apply_async(process_neutron_random, func_args)
             results.append(r)
-        
+
     for r in results:
         r.wait()
-
 
 
 # ==============================================================================
 # Create xml library
 
 lib = openmc.data.DataLibrary()
-lib = lib.from_xml(os.getenv('OPENMC_CROSS_SECTIONS'))        #Gets current
+lib = lib.from_xml(os.getenv("OPENMC_CROSS_SECTIONS"))  # Gets current
 
 for nuc in nucs:
-    outDir = outdirHdf5 + '/' + nuc
-    #fileNum = int(args.samples)
-    for i in range(1,fileNum+1):
-        fileOut = outDir + '/' + nuc + '-' + str(i) + '.h5'
-        lib.register_file(fileOut,nuc + '-' + str(i))
+    outDir = outdirHdf5 / nuc
 
-pre = outdir + '/cross_sections_Pre.xml'
-post = outdir + '/cross_sections_Sandy.xml'
+    for i in range(1, fileNum + 1):
+        fileOut = outDir / (nuc + "-" + str(i) + ".h5")
+        lib.register_file(fileOut, nuc + "-" + str(i))
+
+pre = outdir / "cross_sections_Pre.xml"
+post = outdir / "cross_sections_Sandy.xml"
 
 lib.export_to_xml(pre)
 if os.path.exists(post):
