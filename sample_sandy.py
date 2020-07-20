1b01ec740719760bc2943167dae2d6f3b597eda6
Ander Gray <ander.gray@liverpool.ac.uk>
2020-07-20 13:48:24 +0100

    changes from camel case sandy

--- sample_sandy.py
+++ sample_sandy.py
@@ -16,7 +16,7 @@ import openmc.data
 description = """
 This scripts generates random (gaussian) evaluations of a nuclear data file following 
 its covariance matrix using SANDY, and converts them to HDF5 for use in OpenMC. Script
-generates a cross_sections.xml file with the standard library plus the sampled evaluations.
+generates a cross_sections_sandy.xml file with the standard library plus the sampled evaluations.
 """
 
 
@@ -43,16 +43,16 @@ parser.add_argument("-f", "--format_only", default=False,
                     help="Only format previously sampled files to HDF5")
 
 args = parser.parse_args()
-scriptDir = Path.cwd()
+script_dir = Path.cwd()
 
-outdir = args.outdir
-if outdir == None:
-    outdir = scriptDir / "sandy_rand"
+output_dir = args.outdir
+if output_dir == None:
+    output_dir = script_dir / "sandy_rand"
 else:
-    outdir = Path(outdir).resolve()
+    output_dir = Path(output_dir).resolve()
 
-endf_files_dir = outdir / "endf"
-hdf5_files_dir = outdir / "hdf5"
+endf_files_dir = output_dir / "endf"
+hdf5_files_dir = output_dir / "hdf5"
 
 libdir = args.destination
 if libdir == None:
@@ -70,87 +70,87 @@ format_only = args.format_only
 prefix = "n-"
 suffix = ".endf"
 
-atomicDict = openmc.data.ATOMIC_NUMBER
-nucDict = {}
+atomic_dict = openmc.data.ATOMIC_NUMBER
+nuc_dict = {}
 
 for nuc in nuclides:
-    massNum = int(re.findall("(\d+)", nuc)[0])
-    atomicSym = "".join([i for i in nuc if not i.isdigit()])
-    if atomicSym not in atomicDict.keys():
+    mass_num = int(re.findall("(\d+)", nuc)[0])
+    atomic_sym = "".join([i for i in nuc if not i.isdigit()])
+    if atomic_sym not in atomic_dict.keys():
         print(f"Entered nuclide {nuc} does not have a valid atomic symbol")
         sys.exit()
-    atomicNum = atomicDict[atomicSym]
+    atomic_num = atomic_dict[atomic_sym]
 
-    fileMass = f"{massNum:03}"
-    fileAtomic = f"{atomicNum:03}"
+    file_mass = f"{mass_num:03}"
+    file_atomic = f"{atomic_num:03}"
 
-    fileName = f"{prefix}{fileAtomic}_{atomicSym}_{fileMass}{suffix}"
+    file_name = f"{prefix}{file_atomic}_{atomic_sym}_{file_mass}{suffix}"
 
-    if not (libdir / "neutron" / fileName).isfile():
-        print(f"File {libdir / "neutron" / fileName} does not exist")
+    if not (libdir / "neutron" / file_name).isfile():
+        print(f"File {libdir / "neutron" / file_name} does not exist")
         sys.exit()
-    nucDict[nuc] = {
-        "sym": atomicSym,
-        "massNum": massNum,
-        "atomicNum": atomicNum,
-        "fileName": fileName,
+    nuc_dict[nuc] = {
+        "sym": atomic_sym,
+        "mass_num": mass_num,
+        "atomic_num": atomic_num,
+        "file_name": file_name,
     }
 
 # ==============================================================================
 # GENERATE RANDOM EVALUATIONS OF NUCLEAR DATA USING SANDY
 
 if not format_only:
-    outdir.mkdir(exist_ok=True)
+    output_dir.mkdir(exist_ok=True)
     endf_files_dir.mkdir(exist_ok=True)
 
     for nuc in nuclides:
 
-        nucDirEndf = endf_files_dir / nuc
-        nucDirEndf.mkdir(exist_ok=True)
+        nuc_dir_endf = endf_files_dir / nuc
+        nuc_dir_endf.mkdir(exist_ok=True)
 
         shutil.copyfile(
-            libdir / "neutron" / nucDict[nuc]["fileName"],
-            nucDirEndf / nucDict[nuc]["fileName"],
+            libdir / "neutron" / nuc_dict[nuc]["file_name"],
+            nuc_dir_endf / nuc_dict[nuc]["file_name"],
         )
-        os.chdir(nucDirEndf)
-        sandy_command = f"sandy {nucDict[nuc]["fileName"]} --samples {args.samples} --outname {nuc} --processes {args.processes}"
+        os.chdir(nuc_dir_endf)
+        sandy_command = f"sandy {nuc_dict[nuc]["file_name"]} --samples {args.samples} --outname {nuc} --processes {args.processes}"
         os.system(sandy_command)
 
-    os.chdir(scriptDir)
+    os.chdir(script_dir)
 
 # ==============================================================================
 # CONVERT RANDOM EVALUATIONS TO HDF5
 
 
-def process_neutron_random(nuc, i, outDir, inDir, fileNum):  # Need to add temperatures
+def process_neutron_random(nuc, i, out_dir, in_dir, file_num):  # Need to add temperatures
     """Process ENDF neutron sublibrary file into HDF5 and write into a
     specified output directory."""
 
-    fileIn = inDir / f"{nuc}-{i}"
-    fileOut = outDir / f"{nuc}-{i}.h5"
+    fileIn = in_dir / f"{nuc}-{i}"
+    fileOut = out_dir / f"{nuc}-{i}.h5"
 
     data = openmc.data.IncidentNeutron.from_njoy(fileIn)
     data.name = f"{nuc}-{i}"
     data.export_to_hdf5(fileOut, "w")
     if i % 40 == 0:
-        print(f"Nuclide {nuc} {i+1}/{fileNum} finished")
+        print(f"Nuclide {nuc} {i+1}/{file_num} finished")
 
 
 print("Beginning NJOY processing")
 with Pool() as pool:
     results = []
-    fileNum = int(args.samples)
+    file_num = int(args.samples)
     for nuc in nuclides:
 
-        inDir = endf_files_dir / nuc
-        outDir = hdf5_files_dir / nuc
+        in_dir = endf_files_dir / nuc
+        out_dir = hdf5_files_dir / nuc
 
         hdf5_files_dir.mkdir(exist_ok=True)
-        outDir.mkdir(exist_ok=True)
+        out_dir.mkdir(exist_ok=True)
 
         print(f"Beginning nuclide {nuc} ...")
-        for i in range(1, fileNum + 1):
-            func_args = (nuc, i, outDir, inDir, fileNum)
+        for i in range(1, file_num + 1):
+            func_args = (nuc, i, out_dir, in_dir, file_num)
             r = pool.apply_async(process_neutron_random, func_args)
             results.append(r)
 
@@ -165,13 +165,13 @@ lib = openmc.data.DataLibrary()
 lib = lib.from_xml(os.getenv("OPENMC_CROSS_SECTIONS"))  # Gets current
 
 for nuc in nuclides:
-    outDir = hdf5_files_dir / nuc
-    for i in range(1, fileNum + 1):
-        fileOut = outDir / f"{nuc}-{i}.h5")
+    out_dir = hdf5_files_dir / nuc
+    for i in range(1, file_num + 1):
+        fileOut = out_dir / f"{nuc}-{i}.h5")
         lib.register_file(fileOut)
 
-pre = outdir / "cross_sections_pre.xml"
-post = outdir / "cross_sections_sandy.xml"
+pre = output_dir / "cross_sections_pre.xml"
+post = output_dir / "cross_sections_sandy.xml"
 
 lib.export_to_xml(pre)
 if post.exists():
