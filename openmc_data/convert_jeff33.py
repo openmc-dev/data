20c1b4cc1cc87e57131f8331a993010ca3ca83ee
Jonathan Shimwell <mail@jshimwell.com>
2022-05-11 16:08:35 +0100

    added more convert file testing triggers and activate triggers

--- openmc_data/convert_jeff33.py
+++ openmc_data/convert_jeff33.py
@@ -20,70 +20,95 @@ from .utils import download, extract
 assert sys.version_info >= (3, 6), "Python 3.6+ is required"
 
 
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
-                      argparse.RawDescriptionHelpFormatter):
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter
+):
     pass
 
 
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=CustomFormatter
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=CustomFormatter)
+parser.add_argument(
+    "-d",
+    "--destination",
+    type=Path,
+    default=Path("jeff-3.3-hdf5"),
+    help="Directory to create new library in",
 )
-parser.add_argument('-d', '--destination', type=Path, default=Path('jeff-3.3-hdf5'),
-                    help='Directory to create new library in')
-parser.add_argument('--download', action='store_true',
-                    help='Download tarball from OECD-NEA')
-parser.add_argument('--no-download', dest='download', action='store_false',
-                    help='Do not download tarball from OECD-NEA')
-parser.add_argument('--extract', action='store_true',
-                    help='Extract zip files')
-parser.add_argument('--no-extract', dest='extract', action='store_false',
-                    help='Do not extract .tgz file if it has already been extracted')
-parser.add_argument('--libver', choices=['earliest', 'latest'],
-                    default='earliest', help="Output HDF5 versioning. Use "
-                    "'earliest' for backwards compatibility or 'latest' for "
-                    "performance")
-parser.add_argument('-r', '--release', choices=['3.3'],
-                    default='3.3', help="The nuclear data library release version. "
-                    "The only currently supported option is 3.3.")
-parser.add_argument('--cleanup', action='store_true',
-                    help="Remove download directories when data has "
-                    "been processed")
-parser.add_argument('--no-cleanup', dest='cleanup', action='store_false',
-                    help="Do not remove download directories when data has "
-                    "been processed")
+parser.add_argument(
+    "--download", action="store_true", help="Download tarball from OECD-NEA"
+)
+parser.add_argument(
+    "--no-download",
+    dest="download",
+    action="store_false",
+    help="Do not download tarball from OECD-NEA",
+)
+parser.add_argument("--extract", action="store_true", help="Extract zip files")
+parser.add_argument(
+    "--no-extract",
+    dest="extract",
+    action="store_false",
+    help="Do not extract .tgz file if it has already been extracted",
+)
+parser.add_argument(
+    "--libver",
+    choices=["earliest", "latest"],
+    default="earliest",
+    help="Output HDF5 versioning. Use "
+    "'earliest' for backwards compatibility or 'latest' for "
+    "performance",
+)
+parser.add_argument(
+    "-r",
+    "--release",
+    choices=["3.3"],
+    default="3.3",
+    help="The nuclear data library release version. "
+    "The only currently supported option is 3.3.",
+)
+parser.add_argument(
+    "--cleanup",
+    action="store_true",
+    help="Remove download directories when data has " "been processed",
+)
+parser.add_argument(
+    "--no-cleanup",
+    dest="cleanup",
+    action="store_false",
+    help="Do not remove download directories when data has " "been processed",
+)
 parser.set_defaults(download=True, extract=True, cleanup=False)
 args = parser.parse_args()
 
 
 def main():
 
-    library_name = 'jeff'
+    library_name = "jeff"
 
     cwd = Path.cwd()
 
-    ace_files_dir = cwd.joinpath('-'.join([library_name, args.release, 'ace']))
-    download_path = cwd.joinpath('-'.join([library_name, args.release, 'download']))
+    ace_files_dir = cwd.joinpath("-".join([library_name, args.release, "ace"]))
+    download_path = cwd.joinpath("-".join([library_name, args.release, "download"]))
 
     # This dictionary contains all the unique information about each release. This
     # can be extended to accommodate new releases
     release_details = {
-        '3.3': {
-            'base_url': 'http://www.oecd-nea.org/dbdata/jeff/jeff33/downloads/temperatures/',
-            'compressed_files': [
-                'ace_293.tar.gz',
-                'ace_600.tar.gz',
-                'ace_900.tar.gz',
-                'ace_1200.tar.gz',
-                'ace_1500.tar.gz',
-                'ace_1800.tar.gz',
-                'ace_tsl.tar.gz',
+        "3.3": {
+            "base_url": "http://www.oecd-nea.org/dbdata/jeff/jeff33/downloads/temperatures/",
+            "compressed_files": [
+                "ace_293.tar.gz",
+                "ace_600.tar.gz",
+                "ace_900.tar.gz",
+                "ace_1200.tar.gz",
+                "ace_1500.tar.gz",
+                "ace_1800.tar.gz",
+                "ace_tsl.tar.gz",
             ],
-            'neutron_files': ace_files_dir.rglob('*-[A-Z]*.ace'),
-            'thermal_files': (ace_files_dir / 'ace_tsl').glob('*.ace'),
-            'metastables': ace_files_dir.rglob('*[0-9]m-*.ace'),
-            'compressed_file_size': '7.7 GB',
-            'uncompressed_file_size': '37 GB'
+            "neutron_files": ace_files_dir.rglob("*-[A-Z]*.ace"),
+            "thermal_files": (ace_files_dir / "ace_tsl").glob("*.ace"),
+            "metastables": ace_files_dir.rglob("*[0-9]m-*.ace"),
+            "compressed_file_size": "7.7 GB",
+            "uncompressed_file_size": "37 GB",
         }
     }
 
@@ -95,23 +120,27 @@ def main():
     download_warning = """
     WARNING: This script will download {} of data.
     Extracting and processing the data requires {} of additional free disk space.
-    """.format(details['compressed_file_size'], details['uncompressed_file_size'])
+    """.format(
+        details["compressed_file_size"], details["uncompressed_file_size"]
+    )
 
     if args.download:
         print(download_warning)
-        for f in details['compressed_files']:
-            download(urljoin(details['base_url'], f), output_path=download_path)
+        for f in details["compressed_files"]:
+            download(urljoin(details["base_url"], f), output_path=download_path)
 
     # ==============================================================================
     # EXTRACT FILES FROM TGZ
 
     if args.extract:
         extract(
-            compressed_files=[download_path/ f for f in release_details[args.release]['compressed_files']],
+            compressed_files=[
+                download_path / f
+                for f in release_details[args.release]["compressed_files"]
+            ],
             extraction_dir=ace_files_dir,
             del_compressed_file=args.cleanup,
         )
-
 
     # ==============================================================================
     # CONVERT INCIDENT NEUTRON FILES
@@ -121,80 +150,78 @@ def main():
 
     lib = openmc.data.DataLibrary()
 
-
     def key(p):
         """Return (temperature, atomic number, mass number, metastable)"""
-        z, x, a, temp = p.stem.split('-')
+        z, x, a, temp = p.stem.split("-")
         return int(temp), int(z), int(a[:-1]), a[-1]
 
-
-    for p in sorted((ace_files_dir / 'ace_293').glob('*.ace'), key=key):
-        print(f'Converting: {p}')
+    for p in sorted((ace_files_dir / "ace_293").glob("*.ace"), key=key):
+        print(f"Converting: {p}")
         temp, z, a, m = key(p)
 
         data = openmc.data.IncidentNeutron.from_ace(p)
-        if m == 'm' and not data.name.endswith('_m1'):
+        if m == "m" and not data.name.endswith("_m1"):
             # Correct metastable
             data.metastable = 1
-            data.name += '_m1'
+            data.name += "_m1"
 
-        for T in ('600', '900', '1200', '1500', '1800'):
-            p_add = ace_files_dir / f'ace_{T}' / (p.stem.replace('293', T) + '.ace')
-            print(f'Adding temperature: {p_add}')
+        for T in ("600", "900", "1200", "1500", "1800"):
+            p_add = ace_files_dir / f"ace_{T}" / (p.stem.replace("293", T) + ".ace")
+            print(f"Adding temperature: {p_add}")
             data.add_temperature_from_ace(p_add)
 
-        h5_file = args.destination / f'{data.name}.h5'
-        data.export_to_hdf5(h5_file, 'w', libver=args.libver)
+        h5_file = args.destination / f"{data.name}.h5"
+        data.export_to_hdf5(h5_file, "w", libver=args.libver)
         lib.register_file(h5_file)
 
     # ==============================================================================
     # CONVERT THERMAL SCATTERING FILES
 
     thermal_mats = [
-        'al-sap',
-        'be',
-        'ca-cah2',
-        'd-d2o',
-        'graph',
-        'h-cah2',
-        'h-ch2',
-        'h-h2o',
-        'h-ice',
-        'h-zrh',
-        'mesi',
-        'mg',
-        'o-d2o',
-        'orto-d',
-        'orto-h',
-        'o-sap',
-        'para-d',
-        'para-h',
-        'sili',
-        'tolu',
+        "al-sap",
+        "be",
+        "ca-cah2",
+        "d-d2o",
+        "graph",
+        "h-cah2",
+        "h-ch2",
+        "h-h2o",
+        "h-ice",
+        "h-zrh",
+        "mesi",
+        "mg",
+        "o-d2o",
+        "orto-d",
+        "orto-h",
+        "o-sap",
+        "para-d",
+        "para-h",
+        "sili",
+        "tolu",
     ]
 
-
     def thermal_temp(p):
-        return int(p.stem.split('-')[-1])
+        return int(p.stem.split("-")[-1])
 
-
-    thermal_dir = ace_files_dir / 'ace_tsl'
+    thermal_dir = ace_files_dir / "ace_tsl"
 
     for mat in thermal_mats:
-        for i, p in enumerate(sorted(thermal_dir.glob(f'{mat}*.ace'), key=thermal_temp)):
+        for i, p in enumerate(
+            sorted(thermal_dir.glob(f"{mat}*.ace"), key=thermal_temp)
+        ):
             if i == 0:
-                print(f'Converting: {p}')
+                print(f"Converting: {p}")
                 data = openmc.data.ThermalScattering.from_ace(p)
             else:
-                print(f'Adding temperature: {p}')
+                print(f"Adding temperature: {p}")
                 data.add_temperature_from_ace(p)
 
-        h5_file = args.destination / f'{data.name}.h5'
-        data.export_to_hdf5(h5_file, 'w', libver=args.libver)
+        h5_file = args.destination / f"{data.name}.h5"
+        data.export_to_hdf5(h5_file, "w", libver=args.libver)
         lib.register_file(h5_file)
 
-    lib.export_to_xml(args.destination / 'cross_sections.xml')
+    lib.export_to_xml(args.destination / "cross_sections.xml")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- openmc_data/convert_tendl.py
+++ openmc_data/convert_tendl.py
@@ -19,104 +19,129 @@ from .urls import all_release_details
 assert sys.version_info >= (3, 6), "Python 3.6+ is required"
 
 
-class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter,
-                      argparse.RawDescriptionHelpFormatter):
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter
+):
     pass
 
 
-parser = argparse.ArgumentParser(
-    description=__doc__,
-    formatter_class=CustomFormatter
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=CustomFormatter)
+parser.add_argument(
+    "-d",
+    "--destination",
+    type=Path,
+    default=None,
+    help="Directory to create new library in",
 )
-parser.add_argument('-d', '--destination', type=Path, default=None,
-                    help='Directory to create new library in')
-parser.add_argument('--download', action='store_true',
-                    help='Download files from PSI')
-parser.add_argument('--no-download', dest='download', action='store_false',
-                    help='Do not download files from PSI')
-parser.add_argument('--extract', action='store_true',
-                    help='Extract tar/zip files')
-parser.add_argument('--no-extract', dest='extract', action='store_false',
-                    help='Do not extract tar/zip files')
-parser.add_argument('--libver', choices=['earliest', 'latest'],
-                    default='latest', help="Output HDF5 versioning. Use "
-                    "'earliest' for backwards compatibility or 'latest' for "
-                    "performance")
-parser.add_argument('-r', '--release', choices=['2015', '2017', '2019', '2021'],
-                    default='2021', help="The nuclear data library release "
-                    "version. The currently supported options are 2015, "
-                    "2017, 2019, and 2021.")
-parser.add_argument('--cleanup', action='store_true',
-                    help="Remove download directories when data has "
-                    "been processed")
-parser.add_argument('--no-cleanup', dest='cleanup', action='store_false',
-                    help="Do not remove download directories when data has "
-                    "been processed")
+parser.add_argument("--download", action="store_true", help="Download files from PSI")
+parser.add_argument(
+    "--no-download",
+    dest="download",
+    action="store_false",
+    help="Do not download files from PSI",
+)
+parser.add_argument("--extract", action="store_true", help="Extract tar/zip files")
+parser.add_argument(
+    "--no-extract",
+    dest="extract",
+    action="store_false",
+    help="Do not extract tar/zip files",
+)
+parser.add_argument(
+    "--libver",
+    choices=["earliest", "latest"],
+    default="latest",
+    help="Output HDF5 versioning. Use "
+    "'earliest' for backwards compatibility or 'latest' for "
+    "performance",
+)
+parser.add_argument(
+    "-r",
+    "--release",
+    choices=["2015", "2017", "2019", "2021"],
+    default="2021",
+    help="The nuclear data library release "
+    "version. The currently supported options are 2015, "
+    "2017, 2019, and 2021.",
+)
+parser.add_argument(
+    "--cleanup",
+    action="store_true",
+    help="Remove download directories when data has " "been processed",
+)
+parser.add_argument(
+    "--no-cleanup",
+    dest="cleanup",
+    action="store_false",
+    help="Do not remove download directories when data has " "been processed",
+)
 parser.set_defaults(download=True, extract=True, cleanup=False)
 args = parser.parse_args()
 
 
 def main():
 
-    library_name = 'tendl'
+    library_name = "tendl"
 
     cwd = Path.cwd()
 
-    ace_files_dir = cwd.joinpath('-'.join([library_name, args.release, 'ace']))
-    download_path = cwd.joinpath('-'.join([library_name, args.release, 'download']))
+    ace_files_dir = cwd.joinpath("-".join([library_name, args.release, "ace"]))
+    download_path = cwd.joinpath("-".join([library_name, args.release, "download"]))
     # the destination is decided after the release is known
     # to avoid putting the release in a folder with a misleading name
     if args.destination is None:
-        args.destination = Path('-'.join([library_name, args.release, 'hdf5']))
+        args.destination = Path("-".join([library_name, args.release, "hdf5"]))
 
     # This dictionary contains all the unique information about each release.
     # This can be extended to accommodated new releases
-    release_details = all_release_details[library_name][args.release]['neutron']
+    release_details = all_release_details[library_name][args.release]["neutron"]
 
     download_warning = """
     WARNING: This script will download {} of data.
     Extracting and processing the data requires {} of additional free disk space.
-    """.format(release_details['compressed_file_size'],
-            release_details['uncompressed_file_size'])
+    """.format(
+        release_details["compressed_file_size"],
+        release_details["uncompressed_file_size"],
+    )
 
     # ==============================================================================
     # DOWNLOAD FILES FROM WEBSITE
 
     if args.download:
         print(download_warning)
-        for f in release_details['compressed_files']:
+        for f in release_details["compressed_files"]:
             # Establish connection to URL
-            download(urljoin(release_details['base_url'], f),
-                    output_path=download_path)
+            download(urljoin(release_details["base_url"], f), output_path=download_path)
 
     # ==============================================================================
     # EXTRACT FILES FROM TGZ
 
     if args.extract:
         extract(
-            compressed_files=[download_path/ f for f in release_details['compressed_files']],
+            compressed_files=[
+                download_path / f for f in release_details["compressed_files"]
+            ],
             extraction_dir=ace_files_dir,
-            del_compressed_file=args.cleanup
+            del_compressed_file=args.cleanup,
         )
-
 
     # ==============================================================================
     # CHANGE ZAID FOR METASTABLES
 
-    metastables = ace_files_dir.glob(release_details['metastables'])
+    metastables = ace_files_dir.glob(release_details["metastables"])
     for path in metastables:
-        print('    Fixing {} (ensure metastable)...'.format(path))
-        text = open(path, 'r').read()
+        print("    Fixing {} (ensure metastable)...".format(path))
+        text = open(path, "r").read()
         mass_first_digit = int(text[3])
         if mass_first_digit <= 2:
             text = text[:3] + str(mass_first_digit + 4) + text[4:]
-            open(path, 'w').write(text)
+            open(path, "w").write(text)
 
     # ==============================================================================
     # GENERATE HDF5 LIBRARY -- NEUTRON FILES
 
     # Get a list of all ACE files
-    neutron_files = ace_files_dir.glob(release_details['neutron_files'])
+    neutron_files = ace_files_dir.glob(release_details["neutron_files"])
 
     # Create output directory if it doesn't exist
     args.destination.mkdir(parents=True, exist_ok=True)
@@ -126,28 +151,28 @@ def main():
     for filename in sorted(neutron_files):
 
         # this is a fix for the TENDL-2017 release where the B10 ACE file which has an error on one of the values
-        if args.release == '2017' and filename.name == 'B010':
-            text = open(filename, 'r').read()
-            if text[423:428] == '86843':
-                print('Manual fix for incorrect value in ACE file')
+        if args.release == "2017" and filename.name == "B010":
+            text = open(filename, "r").read()
+            if text[423:428] == "86843":
+                print("Manual fix for incorrect value in ACE file")
                 # see OpenMC user group issue for more details
-                text = ''.join(text[:423])+'86896'+''.join(text[428:])
-                open(filename, 'w').write(text)
+                text = "".join(text[:423]) + "86896" + "".join(text[428:])
+                open(filename, "w").write(text)
 
-        print(f'Converting: {filename}')
+        print(f"Converting: {filename}")
         data = openmc.data.IncidentNeutron.from_ace(filename)
 
         # Export HDF5 file
-        h5_file = args.destination / f'{data.name}.h5'
-        print('Writing {}...'.format(h5_file))
-        data.export_to_hdf5(h5_file, 'w', libver=args.libver)
+        h5_file = args.destination / f"{data.name}.h5"
+        print("Writing {}...".format(h5_file))
+        data.export_to_hdf5(h5_file, "w", libver=args.libver)
 
         # Register with library
         library.register_file(h5_file)
 
     # Write cross_sections.xml
-    library.export_to_xml(args.destination / 'cross_sections.xml')
+    library.export_to_xml(args.destination / "cross_sections.xml")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
