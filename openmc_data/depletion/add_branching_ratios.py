d41389a686e59d3f782c3146a8f2a0734dc6d389
Jonathan Shimwell <mail@jshimwell.com>
2022-05-12 16:21:06 +0100

    prevent script running accidently during testing

--- openmc_data/depletion/add_branching_ratios.py
+++ openmc_data/depletion/add_branching_ratios.py
@@ -1,9 +1,21 @@
+#!/usr/bin/env python
+
+"""
+Writes a depletion chain XML file from a depletion chain XML file and branching
+ratios
+"""
+
 import argparse
 import json
 
 import openmc.deplete
 
-parser = argparse.ArgumentParser()
+class CustomFormatter(
+    argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter
+):
+    pass
+
+parser = argparse.ArgumentParser(description=__doc__, formatter_class=CustomFormatter)
 parser.add_argument('chain_in')
 parser.add_argument('branching_ratios')
 parser.add_argument('chain_out')
@@ -22,3 +34,7 @@ def main():
 
     # Export to new XML file
     chain.export_to_xml(args.chain_out)
+
+
+if __name__ == "__main__":
+    main()
