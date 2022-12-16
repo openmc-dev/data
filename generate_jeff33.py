19b0da889d74e0eaf8ab7d56af65495a1270f652
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 13:20:25 +0000

    Adding tarfile member sanitization to extractall()

--- generate_jeff33.py
+++ generate_jeff33.py
@@ -139,7 +139,26 @@ with tempfile.TemporaryDirectory() as tmpdir:
             elif f.endswith('.tar.gz') or f.endswith('.tgz'):
                 print(f'Extracting {f}...')
                 with tarfile.open(f) as tgz:
-                    tgz.extractall()
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(tgz)
 
     # =========================================================================
     # REPLACE C13 AND O17 WITH FILES FROM TENDL 2019
--- make_compton.py
+++ make_compton.py
@@ -25,7 +25,29 @@ g4dir = Path(f'G4EMLOW{version}')
 if not g4dir.is_dir():
     with tarfile.open(filename, 'r') as tgz:
         print(f'Extracting {filename}...')
-        tgz.extractall()
+        
+        import os
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tgz)
 
 # ==============================================================================
 # GENERATE COMPTON PROFILE HDF5 FILE
