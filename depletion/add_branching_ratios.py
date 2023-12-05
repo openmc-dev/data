07b86a2aaa1ce783d1fa76849a8155ab198327e9
Jonathan Shimwell <mail@jshimwell.com>
2023-12-05 15:32:36 +0000

    branching_ratios_for_multiple_reactions

--- depletion/add_branching_ratios.py
+++ depletion/add_branching_ratios.py
@@ -1,12 +1,13 @@
 import argparse
 import json
+from pathlib import Path
 
 import openmc.deplete
 
 parser = argparse.ArgumentParser()
-parser.add_argument('chain_in')
-parser.add_argument('branching_ratios')
-parser.add_argument('chain_out')
+parser.add_argument('-i', '--chain_in', type=Path, required=True, help='Path of the input chain file')
+parser.add_argument('-b', '--branching_ratios', type=Path, required=True, help='Path of the input branching ratios JSON file')
+parser.add_argument('-o', '--chain_out', type=Path, required=True, help='Path of the produced chain file')
 args = parser.parse_args()
 
 # Load existing chain
@@ -14,8 +15,15 @@ chain = openmc.deplete.Chain.from_xml(args.chain_in)
 
 # Set branching ratios
 with open(args.branching_ratios) as fh:
-    br = json.load(fh)
-chain.set_branch_ratios(br, strict=False)
+    all_branch_ratios = json.load(fh)
+
+for reaction, branch_ratios in all_branch_ratios.items():
+
+    chain.set_branch_ratios(
+        branch_ratios=branch_ratios,
+        reaction=reaction,
+        strict=False
+    )
 
 # Export to new XML file
 chain.export_to_xml(args.chain_out)
